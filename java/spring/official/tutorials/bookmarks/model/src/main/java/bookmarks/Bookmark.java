4be6dcb16abc51f2165bcbf0c737cf2614dd0f19
alex <a.juncevich@gmail.com>
2017-01-10 19:24:49 +0500

    Create Bookmark entity.

--- java/spring/official/tutorials/bookmarks/model/src/main/java/bookmarks/Bookmark.java
+++ java/spring/official/tutorials/bookmarks/model/src/main/java/bookmarks/Bookmark.java
@@ -1,7 +1,49 @@
 package bookmarks;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 /**
  * Created by alex on 10.01.17.
  */
+@Entity
 public class Bookmark {
+
+    public String uri;
+    public String decription;
+    @JsonIgnore
+    @ManyToOne
+    private Account account;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Bookmark() {
+    }
+
+    public Bookmark(Account account, String uri, String decription) {
+        this.account = account;
+        this.uri = uri;
+        this.decription = decription;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
 }
