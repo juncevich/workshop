de773c70db27bd010b0608b05a647014684f7a57
alex <a.juncevich@gmail.com>
2017-01-23 20:17:52 +0500

    initial commit

---
+++ java/spring/unofficial/grabduck/src/main/java/persistence/StackoverflowWebsiteRepository.java
@@ -0,0 +1,11 @@
+package persistence;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by alex on 23.01.17.
+ */
+@Repository
+public class StackoverflowWebsiteRepository {
+
+}
