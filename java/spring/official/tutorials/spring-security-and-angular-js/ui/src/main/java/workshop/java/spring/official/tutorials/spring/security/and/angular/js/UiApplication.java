d0fc479921eeb00a8360c93be0e14746cf98e3f7
alex <a.juncevich@gmail.com>
2017-03-19 20:28:27 +0500

    add some files

---
+++ java/spring/official/tutorials/spring-security-and-angular-js/ui/src/main/java/workshop/java/spring/official/tutorials/spring/security/and/angular/js/UiApplication.java
@@ -0,0 +1,12 @@
+package workshop.java.spring.official.tutorials.spring.security.and.angular.js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+}
