37c55dea8e51ec9accd0fcf74c8cb9334e3b190c
alex <a.juncevich@gmail.com>
2017-03-20 23:48:13 +0500

    Fille systemjs.config.js

--- Angular2/SportsStore/systemjs.config.js
+++ Angular2/SportsStore/systemjs.config.js
@@ -1,3 +1,20 @@
 /**
  * Created by alex on 20.03.17.
  */
+(function (global) {
+    var paths = {
+        "@angular/*": "node_modules/@angular/*"
+    }
+
+    var packages = {"app": {}};
+
+    var angularModules = ["common", "compiler", "core", "platform-browser", "platform-browser-dunamic", "forms", "http", "router"];
+
+    angularModules.forEach(function (pkg) {
+        packages["@angular/" + pkg] = {
+            main: "/bundles/" + pkg + ".umd.min.js"
+        };
+    });
+
+    System.config({paths: paths, packages: packages});
+})(this);
