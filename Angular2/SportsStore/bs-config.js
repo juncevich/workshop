1d5bebf1bb4b1558de50dcf8ddbf94297e748b7c
alex <a.juncevich@gmail.com>
2017-03-20 23:38:43 +0500

    Fill bs-config.js.

--- Angular2/SportsStore/bs-config.js
+++ Angular2/SportsStore/bs-config.js
@@ -1,3 +1,10 @@
 /**
  * Created by alex on 20.03.17.
  */
+module.exports = {
+    ghostMode: false,
+    reloadDelay: 1000,
+    reloadDebounce: 1000,
+    injectChanges: false,
+    minify: false
+}
