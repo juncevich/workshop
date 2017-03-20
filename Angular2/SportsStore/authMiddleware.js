c16f16a89e3938a02ff681df057abfe1763545ac
alex <a.juncevich@gmail.com>
2017-03-20 23:14:19 +0500

    Fill authMiddleware

--- Angular2/SportsStore/authMiddleware.js
+++ Angular2/SportsStore/authMiddleware.js
@@ -1,3 +1,35 @@
 /**
  * Created by alex on 19.03.17.
  */
+const jwt = require("jsonwebtoken");
+
+const APP_SECRET = "myappsecret";
+const USERNAME = "admin";
+const PASSWORD = "secret";
+
+module.exports = function (req, res, next) {
+    if (req.url == "/login" && req.method == "POST") {
+        if (req.body != null && req.body.name == USERNAME && req.body.password == PASSWORD) {
+            let token = jwt.sign({data: USERNAME, expiresIn: "1h"}, APP_SECRET);
+            res.json({success: true, token: token});
+        } else {
+            res.json({success: false});
+        }
+        res.end;
+        return;
+    } else if ((req.url.startWith("/products") && req.method != "GET") || (req.url.startsWith("/orders") && req.method!="POST")) {
+        let token = req.headers["authorization"];
+        if (token != null && token.startWith("Bearer<")) {
+            token = token.substring(7, token.length -1);
+            try {
+                jwt.verify(token, APP_SECRET);
+                next();
+                return;
+            } catch (err) {}
+        }
+        res.statusCode = 401;
+        res.end();
+        return;
+    }
+    next();
+}
