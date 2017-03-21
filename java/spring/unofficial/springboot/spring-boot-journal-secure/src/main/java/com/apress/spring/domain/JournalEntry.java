07bdf2a4e66f8486cf9ce3cecfd11f2cd303dedd
alex <a.juncevich@gmail.com>
2017-03-21 23:23:05 +0500

    Add spring boot project

---
+++ java/spring/unofficial/springboot/spring-boot-journal-secure/src/main/java/com/apress/spring/domain/JournalEntry.java
@@ -0,0 +1,8 @@
+package com.apress.spring.domain;
+
+/**
+ * Created by alex on 21.03.17.
+ */
+public class JournalEntry {
+
+}
---
+++ java/spring/unofficial/springboot/spring-boot-journal-secure/src/main/java/com/apress/spring/utils/JsonDateSerializer.java
@@ -0,0 +1,8 @@
+package com.apress.spring.utils;
+
+/**
+ * Created by alex on 21.03.17.
+ */
+public class JsonDateSerializer {
+
+}
