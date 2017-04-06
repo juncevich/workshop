23f5bcca107fb124deb6f84f9b1f250566b15584
alex <a.juncevich@gmail.com>
2017-04-06 18:45:03 +0500

    modify template

--- Angular2/official/quickstart/src/app/app.component.ts
+++ Angular2/official/quickstart/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}. It\`s test note</h1>`,
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
 })
 export class AppComponent {
   title = 'Tour of Heroes';
