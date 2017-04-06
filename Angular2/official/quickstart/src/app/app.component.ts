b8224149b98b36dd4b61015e0a024b5ea712f56d
alex <a.juncevich@gmail.com>
2017-04-06 18:41:28 +0500

    add title and hero feilds in AppComponent

--- Angular2/official/quickstart/src/app/app.component.ts
+++ Angular2/official/quickstart/src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}. It\`s test note</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
+}
