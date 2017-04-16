ea89a32ba7064aceff906876f5dde6dad56b71b0
alex <a.juncevich@gmail.com>
2017-04-16 23:09:09 +0500

    Some refactoring

--- Angular2/official/quickstart/src/app/components/hero-detail.component.ts
+++ Angular2/official/quickstart/src/app/components/hero-detail.component.ts
@@ -2,12 +2,12 @@
  * Created by alex on 11.04.17.
  */
 import 'rxjs/add/operator/switchMap';
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
-import {Location} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
-import {Hero} from '../hero';
-import {HeroService} from '../services/hero.service';
+import { Hero } from '../hero';
+import { HeroService } from '../services/hero.service';
 @Component({
   selector: 'hero-detail',
   templateUrl: '../../view/hero-detail.component.html',
@@ -17,8 +17,8 @@ export class HeroDetailComponent implements OnInit {
   hero: Hero;
 
   constructor(private heroService: HeroService,
-              private route: ActivatedRoute,
-              private location: Location) {
+    private route: ActivatedRoute,
+    private location: Location) {
   }
 
   ngOnInit(): void {
