a260e88f7d66d3662a12dc9ed53d2509ddb9a5f2
alex <a.juncevich@gmail.com>
2017-04-17 23:49:09 +0500

    Add inmemory web api

--- Angular2/official/quickstart/src/app/app.module.ts
+++ Angular2/official/quickstart/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service'
 import { AppComponent } from './components/app.component';
 import { HeroDetailComponent } from './components/hero-detail.component';
 import { HeroesComponent } from './components/heroes.component';
@@ -12,6 +15,8 @@ import { DashboardComponent } from './components/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
       {
         path: '',
