fea9de8c58834d10871e56f89ac814c1338dfb7a
alex <a.juncevich@gmail.com>
2017-04-19 18:46:09 +0500

    Add HeroSearchService

---
+++ Angular2/official/quickstart/src/app/services/hero-search.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Hero } from '../hero';
+
+@Injectable()
+export class HeroSearchService {
+
+}
