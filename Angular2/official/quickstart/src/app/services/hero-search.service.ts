10efde576e09e3bd9f047fc0db6525d6e081f021
alex <a.juncevich@gmail.com>
2017-04-19 18:50:12 +0500

    Add search method

--- Angular2/official/quickstart/src/app/services/hero-search.service.ts
+++ Angular2/official/quickstart/src/app/services/hero-search.service.ts
@@ -6,5 +6,9 @@ import { Hero } from '../hero';
 
 @Injectable()
 export class HeroSearchService {
+    constructor(private http: Http) { }
 
+    search(term: string): Observable<Hero[]> {
+        return this.http.get('app/heroes/?name=${term}').map(response => response.json().data as Hero[]);
+    }
 }
