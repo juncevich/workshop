87c861b0d0503c8380305cb2f76e8ebf0ad30e1b
alex <a.juncevich@gmail.com>
2017-04-19 18:41:19 +0500

    Add delete functional

--- Angular2/official/quickstart/src/app/services/hero.service.ts
+++ Angular2/official/quickstart/src/app/services/hero.service.ts
@@ -33,6 +33,11 @@ export class HeroService {
       .then(() => hero)
       .catch(this.handleError);
   }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, { headers: this.headers }).toPromise().then(() => null).catch(this.handleError);
+  }
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
