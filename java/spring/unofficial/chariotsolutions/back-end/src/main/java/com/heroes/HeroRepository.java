8f02ca77c767d248db299587c75d9ed0c65d227b
alex <a.juncevich@gmail.com>
2017-04-25 00:39:34 +0500

    create named query

--- java/spring/unofficial/chariotsolutions/back-end/src/main/java/com/heroes/HeroRepository.java
+++ java/spring/unofficial/chariotsolutions/back-end/src/main/java/com/heroes/HeroRepository.java
@@ -1,8 +1,17 @@
 package com.heroes;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 /**
  * Created by alex on 25.04.17.
  */
-public class HeroRepository {
+@RepositoryRestResource(collectionResourceRel = "heroes",
+        path = "heroes")
+public interface HeroRepository extends CrudRepository<Hero, Long> {
 
+    @Query("select h from Hero h where lower(h.name) like CONCAT('%', lower(:name), '%')")
+    Iterable<Hero> findByName(@Param("name") String name);
 }
