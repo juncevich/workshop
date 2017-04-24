cf03cc5d632607405ecdde2ce37f00a437401078
alex <a.juncevich@gmail.com>
2017-04-25 00:21:02 +0500

    Add Lombok

--- java/spring/unofficial/chariotsolutions/back-end/src/main/java/com/heroes/Hero.java
+++ java/spring/unofficial/chariotsolutions/back-end/src/main/java/com/heroes/Hero.java
@@ -1,8 +1,17 @@
 package com.heroes;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by alex on 25.04.17.
  */
+@Entity
 public class Hero {
-
+    @Id
+    @GeneratedValue
+    private long id;
+    
+    
 }
