ec13d234ecbcc251e38d25386f11917bdb322657
alex <a.juncevich@gmail.com>
2017-04-25 00:28:51 +0500

    Configure RepositoryConfig

--- java/spring/unofficial/chariotsolutions/back-end/src/main/java/com/heroes/RepositoryConfig.java
+++ java/spring/unofficial/chariotsolutions/back-end/src/main/java/com/heroes/RepositoryConfig.java
@@ -1,8 +1,18 @@
 package com.heroes;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
 /**
  * Created by alex on 25.04.17.
  */
-public class RepositoryConfig {
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter{
 
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        config.exposeIdsFor(Hero.class);
+    }
 }
