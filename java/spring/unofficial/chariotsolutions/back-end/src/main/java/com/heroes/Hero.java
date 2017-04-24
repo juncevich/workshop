92423085357aa95a83c6e542b0121e56d635bd33
alex <a.juncevich@gmail.com>
2017-04-25 00:23:22 +0500

    Add fields to Hero class

--- java/spring/unofficial/chariotsolutions/back-end/src/main/java/com/heroes/Hero.java
+++ java/spring/unofficial/chariotsolutions/back-end/src/main/java/com/heroes/Hero.java
@@ -4,14 +4,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 /**
  * Created by alex on 25.04.17.
  */
 @Entity
+@Data
 public class Hero {
+
     @Id
     @GeneratedValue
     private long id;
-    
-    
+
+    private String name;
+
 }
