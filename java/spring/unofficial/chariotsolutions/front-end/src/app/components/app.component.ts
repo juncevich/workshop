caf32ff6bc864175b1f57929e2fa8f1f2bcf84f0
Alexandr Yuncevich <a.juncevich@gmail.com>
2017-04-29 17:48:29 +0500

    Add files

---
+++ java/spring/unofficial/chariotsolutions/front-end/src/app/components/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: '../../view/app.component.html',
+  styleUrls: ['../../styles/app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
