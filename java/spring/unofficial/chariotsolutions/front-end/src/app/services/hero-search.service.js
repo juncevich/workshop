b57203e658077a9f8fbe3a3a1830f94870143f4b
Alexandr Yuncevich <a.juncevich@gmail.com>
2017-04-30 18:43:30 +0500

    modify hero-search service

--- java/spring/unofficial/chariotsolutions/front-end/src/app/services/hero-search.service.js
+++ java/spring/unofficial/chariotsolutions/front-end/src/app/services/hero-search.service.js
@@ -16,7 +16,7 @@ var HeroSearchService = (function () {
         this.http = http;
     }
     HeroSearchService.prototype.search = function (term) {
-        return this.http.get('http://localhost:8080/heroes/?name=${term}').map(function (response) { return response.json().data; });
+        return this.http.get('http://localhost:8080/heroes/search/findByName?name=${term}').map(function (response) { return response.json(); });
     };
     return HeroSearchService;
 }());
