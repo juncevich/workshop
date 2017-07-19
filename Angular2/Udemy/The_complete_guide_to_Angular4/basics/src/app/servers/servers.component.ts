1f41e3941f7957f3952fd4bdf395c939e3de4e3e
juncevich <a.juncevich@gmail.com>
2017-07-19 19:17:06 +0500

    Add server name to creation message.

--- Angular2/Udemy/The_complete_guide_to_Angular4/basics/src/app/servers/servers.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/basics/src/app/servers/servers.component.ts
@@ -21,7 +21,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Server name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
