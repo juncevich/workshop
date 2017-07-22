c2f75708df62b16ba1a320cbf0c62b9ba9e07d57
juncevich <a.juncevich@gmail.com>
2017-07-22 19:26:59 +0500

    Add @ViewChild

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,8 +10,9 @@ export class CockpitComponent implements OnInit {
   serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated')
   blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput;
 
   constructor() {
   }
@@ -23,14 +24,15 @@ export class CockpitComponent implements OnInit {
     this.serverCreated.emit(
       {
         serverName: nameInput.value,
-        serverContent: this.newServerContent
-      });
+        serverContent: this.serverContentInput.nativeElement.value
+      })
+    ;
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
