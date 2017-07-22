99bc6a04fe03ba6e1b6fdbb2cf857c093a85b926
juncevich <a.juncevich@gmail.com>
2017-07-22 18:44:21 +0500

    Add custom event

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/cmp-databinding-start/src/app/app.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/cmp-databinding-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test !'}];
 
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 
 }
