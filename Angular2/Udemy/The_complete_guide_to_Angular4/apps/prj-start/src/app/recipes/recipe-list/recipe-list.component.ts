2635ca1627ff36d6ef2fdfd69591ccb2fd663cf7
juncevich <a.juncevich@gmail.com>
2017-07-23 13:21:14 +0500

    Add adding ingredients to shopping list

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,11 +7,12 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simply test',
+    new Recipe('First Recipe', 'This is a first simply test',
       'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-    new Recipe('A Test Recipe', 'This is a simply test',
-      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+    new Recipe('Second Recipe', 'This is a second simply test',
+      'https://www.heartandstroke.ca/-/media/images/recipes/en/stir-fried-broccoli--red-peppers-and-beef-on-a-white-plate-with-chopsticks.ashx?bc=f7f7f7&w=1160&h=653&as=1&la=en&hash=8BD99241B605ADFE8621A0EC7427DA7FB492AD27')
   ];
 
   constructor() {
@@ -20,4 +21,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
