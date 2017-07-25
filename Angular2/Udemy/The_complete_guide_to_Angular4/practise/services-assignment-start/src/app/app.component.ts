0f6eb26fa82152ba3f95998f6ecdcecfadad5984
juncevich <a.juncevich@gmail.com>
2017-07-25 18:57:53 +0500

    move methods to user service

--- Angular2/Udemy/The_complete_guide_to_Angular4/practise/services-assignment-start/src/app/app.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/practise/services-assignment-start/src/app/app.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
+export class AppComponent implements OnInit {
+  activeUsers: string[] = [];
+  inactiveUsers: string[] = [];
+
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.activeUsers = this.userService.activeUsers;
+    this.inactiveUsers = this.userService.inactiveUsers;
+  }
 
   onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
+    this.userService.addToInactive(id);
   }
 
   onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
+    this.userService.addToActive(id);
   }
 }
--- Angular2/Udemy/The_complete_guide_to_Angular4/practise/services-assignment-start/src/app/user.service.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/practise/services-assignment-start/src/app/user.service.ts
@@ -2,4 +2,16 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class UserService {
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
+
+  addToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
+
+  addToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
 }
