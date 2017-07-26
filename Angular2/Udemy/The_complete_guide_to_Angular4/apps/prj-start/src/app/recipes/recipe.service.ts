0caa1ac0a53e834deac70fa1e8e22772054ec8fd
juncevich <a.juncevich@gmail.com>
2017-07-26 20:39:08 +0500

    Add ingredients to recipe details

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/prj-start/src/app/recipes/recipe.service.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/prj-start/src/app/recipes/recipe.service.ts
@@ -1,16 +1,17 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('First Recipe', 'This is a first simply test',
-      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg', [new Ingredient('Meat', 1)]),
     new Recipe('Second Recipe', 'This is a second simply test',
       'https://www.heartandstroke.ca/-/media/images/recipes/en/stir-fried-broccoli--red-peppers-and-beef' +
       '-on-a-white-plate-with-chopsticks.ashx?bc=f7f7f7&w=1160&h=653&as=1&la=en&hash=8BD99241B605ADFE8621' +
-      'A0EC7427DA7FB492AD27')
+      'A0EC7427DA7FB492AD27', [new Ingredient('Buns', 2), new Ingredient('Meat', 1)])
   ];
 
   getRecipes() {
