fc7002b01b7777b10358548dceb56572825bb3d6
juncevich <a.juncevich@gmail.com>
2017-07-26 20:02:55 +0500

    Some logic to shopping list service

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/prj-start/src/app/shopping-list/shopping-list.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/prj-start/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,15 +8,13 @@ import {Ingredient} from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
 
-
-  constructor() {
+  constructor(private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-  }
 }
