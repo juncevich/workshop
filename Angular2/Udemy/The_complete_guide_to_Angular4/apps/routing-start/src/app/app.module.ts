fd5aa05342d9cf3e80d46ff684f0db64279fb2f2
juncevich <a.juncevich@gmail.com>
2017-07-27 23:40:39 +0500

    Add parameters

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/routing-start/src/app/app.module.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/routing-start/src/app/app.module.ts
@@ -17,7 +17,8 @@ const appRoute: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UserComponent},
   {path: 'users/:id/:name', component: UserComponent},
-  {path: 'servers', component: ServersComponent}
+  {path: 'servers', component: ServersComponent},
+  {path: 'servers/:id/edit', component: EditServerComponent},
 ];
 
 @NgModule({
