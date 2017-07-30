0bc0b77a0a613e9c7fbdb0ca6e4e1afdb7237eb5
juncevich <a.juncevich@gmail.com>
2017-07-30 14:21:24 +0500

    Add guard module

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/routing-start/src/app/app.module.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/routing-start/src/app/app.module.ts
@@ -14,6 +14,8 @@ import {ServersService} from './servers/servers.service';
 import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {AuthGuard} from "./auth-guard.service";
+import {AuthService} from "./auth.service";
 
 
 @NgModule({
@@ -33,7 +35,7 @@ import {AppRoutingModule} from "./app-routing.module";
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
