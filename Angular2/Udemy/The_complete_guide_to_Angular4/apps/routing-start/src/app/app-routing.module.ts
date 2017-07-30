1305641c1adfca96f6ee9789789a3e1023e3abc9
juncevich <a.juncevich@gmail.com>
2017-07-30 14:28:18 +0500

    Protect child routes

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/routing-start/src/app/app-routing.module.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/routing-start/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
-import {HomeComponent} from "./home/home.component";
-import {UsersComponent} from "./users/users.component";
-import {UserComponent} from "./users/user/user.component";
-import {ServersComponent} from "./servers/servers.component";
-import {ServerComponent} from "./servers/server/server.component";
-import {EditServerComponent} from "./servers/edit-server/edit-server.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {AuthGuard} from "./auth-guard.service";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {UsersComponent} from './users/users.component';
+import {UserComponent} from './users/user/user.component';
+import {ServersComponent} from './servers/servers.component';
+import {ServerComponent} from './servers/server/server.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AuthGuard} from './auth-guard.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -17,7 +17,11 @@ const appRoutes: Routes = [
   ]
   },
   {
-    path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
+    path: 'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
     {path: ':id', component: ServerComponent},
     {path: ':id/edit', component: EditServerComponent}
   ]
