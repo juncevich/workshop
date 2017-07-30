e625802f6d19c77ad804cc86fbe4edd3e2915dd7
juncevich <a.juncevich@gmail.com>
2017-07-30 17:44:07 +0500

    Config route parameters

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/prj-start/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/prj-start/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,16 +9,8 @@ import {RecipeService} from "../../recipe.service";
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
-
-
-  constructor(private recipeService: RecipeService) {
-  }
 
   ngOnInit() {
   }
 
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
