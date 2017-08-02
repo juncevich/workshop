fa68548369465a62163695ab39b6e744616c8fc0
juncevich <a.juncevich@gmail.com>
2017-08-03 00:12:03 +0500

    Finish own observabke

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/observables-start/src/app/home/home.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/observables-start/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
 
+import {Observer} from 'rxjs/Observer';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -20,7 +22,42 @@ export class HomeComponent implements OnInit {
     //   }
     // );
 
-    
+    const myObservable = Observable.create((observer: Observer<string>) => {
+        setTimeout(
+          () => {
+            observer.next('first page');
+          }, 2000
+        );
+        setTimeout(
+          () => {
+            observer.next('second page');
+          }, 4000
+        );
+        setTimeout(
+          () => {
+            observer.complete();
+            // observer.error('This does not work');
+          }, 5000
+        );
+        setTimeout(
+          () => {
+            observer.next('third page');
+          }, 4000
+        );
+      }
+    );
+
+    myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('Completed!');
+      }
+    );
   }
 
 }
