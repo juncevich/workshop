a93ab5ba406501a2eba6f9330e9977abba20535e
juncevich <a.juncevich@gmail.com>
2017-08-06 15:03:49 +0500

    Solve practise

--- Angular2/Udemy/The_complete_guide_to_Angular4/practise/forms-td-assignment-start/src/app/app.component.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/practise/forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedDescription = 'Advanced';
+
+  @ViewChild('signupForm') sgnForm: NgForm;
+
+  onSubmit() {
+    console.log(this.sgnForm.value);
+  }
 }
