ee8f5e4eafb5d496c64b0a301b1a9c9d1ded31bf
juncevich <a.juncevich@gmail.com>
2017-08-06 12:10:37 +0500

    Add user activating

--- Angular2/Udemy/The_complete_guide_to_Angular4/apps/observables-start/src/app/app.module.ts
+++ Angular2/Udemy/The_complete_guide_to_Angular4/apps/observables-start/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {UserComponent} from './user/user.component';
+import {AppRoutingModule} from './app-routing.module';
+import {UsersService} from "./users.service";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
