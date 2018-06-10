09a36af79113673f44aa27f07f16066a447229a2
juncevich <a.juncevich@gmail.com>
2018-06-10 18:43:35 +0500

    Readd Kotlin app

--- kotlin/kotlin-rest/src/test/kotlin/com/example/kotlinrest/KotlinRestApplicationTests.kt
+++ kotlin/kotlin-rest/src/test/kotlin/com/example/kotlinrest/KotlinRestApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package com.example.kotlinrest
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class SimpleRestApplicationTests {
+class KotlinRestApplicationTests {
 
     @Test
     fun contextLoads() {
