74436dac5d5aee0c2b67fb3c20dac0270c98d227
juncevich <a.juncevich@gmail.com>
2018-07-01 23:19:26 +0500

    Add saveTask()

--- java/spring/unofficial/udemy/spring+angular/taskapp/src/main/frontend/src/app/tasks/task.service.ts
+++ java/spring/unofficial/udemy/spring+angular/taskapp/src/main/frontend/src/app/tasks/task.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {Task} from "src/app/tasks/task.model";
 
 @Injectable()
 export class TaskService {
@@ -12,5 +13,10 @@ export class TaskService {
         return this
             .http
             .get('/api/tasks');
+    }
+
+    saveTask(task: Task, checked: boolean) {
+        task.completed = checked;
+        return this.http.post('/api/task/save', task);
     }
 }
