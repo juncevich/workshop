e299196212a51b69738202fb7414bc5fe0e49f05
juncevich <a.juncevich@gmail.com>
2018-07-01 22:05:10 +0500

    Add labels

--- java/spring/unofficial/udemy/spring+angular/taskapp/src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ java/spring/unofficial/udemy/spring+angular/taskapp/src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks: Task[] = [];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.tasks.push(new Task(1, "Task 1", true, "07/08/17"));
+        this.tasks.push(new Task(2, "Task 2", false, "07/08/17"));
+        this.tasks.push(new Task(3, "Task 3", false, "07/08/17"));
+    }
+
+    getDueDateLabel(task: Task) {
+        return task.completed ? 'badge-success' : 'badge-primary';
+    }
+
+    onTaskChange(event, task) {
+        console.log('Task has changed')
+        // this.taskService.saveTask(task, event.target.checked).subscribe();
+    }
+
 
 }
