b0c87ce255c89f2e5f1a7c48b0c87c6520a2d159
Alexandr Juncevich <a.juncevich@gmail.com>
2018-11-25 16:41:06 +0500

    Refactoring

--- java/nio/src/main/java/SingleThreadedBlockingServer.java
+++ java/nio/src/main/java/SingleThreadedBlockingServer.java
@@ -1,27 +1,39 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+@Slf4j
 public class SingleThreadedBlockingServer {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(8080);
         while (true) {
             Socket s = ss.accept();
-            InputStream in = s.getInputStream();
-            OutputStream out = s.getOutputStream();
+            handle(s);
+        }
+    }
+
+    private static void handle(Socket s) throws IOException {
+        log.info("Connected to {}", s);
+        try (
+                s;
+                InputStream in = s.getInputStream();
+                OutputStream out = s.getOutputStream()
+        ) {
             int data;
-            while ((data = in.read())!=-1){
+            while ((data = in.read()) != -1) {
                 out.write(transmogrify(data));
             }
-            out.close();
-            in.close();
-            s.close();
+
+        } finally {
+            log.info("Disconnected from {}", s);
         }
     }
 
     private static int transmogrify(int data) {
-        return Character.isLetter(data)? data ^ ' ': data;
+        return Character.isLetter(data) ? data ^ ' ' : data;
     }
 }
