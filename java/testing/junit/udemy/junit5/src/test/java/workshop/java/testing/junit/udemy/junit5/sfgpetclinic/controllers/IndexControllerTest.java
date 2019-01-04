01f1d32bfea56d3b5425b95c2b8dde6bddaff211
Alexandr Juncevich <a.juncevich@gmail.com>
2019-01-04 14:43:22 +0500

    Add timeout assertion

--- java/testing/junit/udemy/junit5/src/test/java/workshop/java/testing/junit/udemy/junit5/sfgpetclinic/controllers/IndexControllerTest.java
+++ java/testing/junit/udemy/junit5/src/test/java/workshop/java/testing/junit/udemy/junit5/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package workshop.java.testing.junit.udemy.junit5.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 class IndexControllerTest {
 
@@ -26,5 +31,25 @@ class IndexControllerTest {
     @DisplayName("Test exception ")
     void oopsHandler() {
         assertThrows(ValueNotFoundException.class, () -> controller.oopsHandler());
+    }
+
+    @Test
+    @Disabled("Demo timeout")
+    void testTimeout() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+
+            System.out.println("I got here");
+        });
+    }
+
+    @Test
+    @Disabled("Demo timeout")
+    void testTimeoutPrempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+
+            System.out.println("I got here 2    ");
+        });
     }
 }
