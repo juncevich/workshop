27c9934f88247398506d15ea10ec5ee108d75e23
Alexandr Juncevich <a.juncevich@gmail.com>
2019-01-05 16:45:44 +0500

    Add test extension

--- java/testing/junit/udemy/junit5/src/test/java/workshop/java/testing/junit/udemy/junit5/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ java/testing/junit/udemy/junit5/src/test/java/workshop/java/testing/junit/udemy/junit5/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -2,7 +2,10 @@ package workshop.java.testing.junit.udemy.junit5.sfgpetclinic.services.springdat
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import workshop.java.testing.junit.udemy.junit5.sfgpetclinic.extensions.TimingExtension;
 
+@ExtendWith(TimingExtension.class)
 class PetTypeSDJpaServiceIT {
 
     @BeforeEach
