d9147aebd25a4165baf6d9d7d2d9c9dc8c0c9a8a
Alexandr Juncevich <a.juncevich@gmail.com>
2019-01-08 12:58:41 +0500

    Add argument matcher

--- java/testing/junit/udemy/junit5/src/test/java/workshop/java/testing/junit/udemy/junit5/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ java/testing/junit/udemy/junit5/src/test/java/workshop/java/testing/junit/udemy/junit5/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -12,6 +12,8 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.never;
@@ -28,6 +30,14 @@ class SpecialitySDJpaServiceTest {
     private SpecialitySDJpaService service;
 
     @Test
+    void testDeleteByObject() {
+        Speciality speciality = new Speciality();
+        service.delete(speciality);
+        verify(specialtyRepository).delete(any(Speciality.class));
+    }
+
+
+    @Test
     void findByIdTest() {
         Speciality speciality = new Speciality();
         when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
@@ -35,7 +45,7 @@ class SpecialitySDJpaServiceTest {
         Speciality foundSpeciality = service.findById(1L);
         assertThat(foundSpeciality).isNotNull();
 
-        verify(specialtyRepository).findById(1L);
+        verify(specialtyRepository).findById(anyLong());
         assertEquals(speciality, foundSpeciality);
     }
 
