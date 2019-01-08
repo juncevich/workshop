d9ea5c7a5d2a0602c8d824a627970c62c26accbb
Alexandr Juncevich <a.juncevich@gmail.com>
2019-01-08 12:52:33 +0500

    Returning values in the mocks

--- java/testing/junit/udemy/junit5/src/test/java/workshop/java/testing/junit/udemy/junit5/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ java/testing/junit/udemy/junit5/src/test/java/workshop/java/testing/junit/udemy/junit5/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,11 +8,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import workshop.java.testing.junit.udemy.junit5.sfgpetclinic.model.Speciality;
 import workshop.java.testing.junit.udemy.junit5.sfgpetclinic.repositories.SpecialtyRepository;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -21,6 +26,18 @@ class SpecialitySDJpaServiceTest {
 
     @InjectMocks
     private SpecialitySDJpaService service;
+
+    @Test
+    void findByIdTest() {
+        Speciality speciality = new Speciality();
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpeciality = service.findById(1L);
+        assertThat(foundSpeciality).isNotNull();
+
+        verify(specialtyRepository).findById(1L);
+        assertEquals(speciality, foundSpeciality);
+    }
 
     @Test
     void deleteById() {
