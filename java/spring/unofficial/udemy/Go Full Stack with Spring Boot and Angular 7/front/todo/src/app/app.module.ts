3ff9d763b29bd7d4c2665851734dff2f3290ed53
Alexandr Juncevich <a.juncevich@gmail.com>
2019-01-12 12:40:07 +0500

    Create todos list component

--- java/spring/unofficial/udemy/Go Full Stack with Spring Boot and Angular 7/front/todo/src/app/app.module.ts
+++ java/spring/unofficial/udemy/Go Full Stack with Spring Boot and Angular 7/front/todo/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRouterModule } from './app-routing.module';
 import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     WelcomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    ListTodosComponent
   ],
   imports: [
     BrowserModule,
