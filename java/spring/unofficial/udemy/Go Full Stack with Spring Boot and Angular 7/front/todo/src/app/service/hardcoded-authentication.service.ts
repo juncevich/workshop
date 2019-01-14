b5f604cdc1cf72717a41d3b667c450cdaabfbaf8
Alexandr Juncevich <a.juncevich@gmail.com>
2019-01-15 00:20:24 +0500

    clean code in service

--- java/spring/unofficial/udemy/Go Full Stack with Spring Boot and Angular 7/front/todo/src/app/service/hardcoded-authentication.service.ts
+++ java/spring/unofficial/udemy/Go Full Stack with Spring Boot and Angular 7/front/todo/src/app/service/hardcoded-authentication.service.ts
@@ -8,18 +8,18 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password) {
-    console.log('before ' + this.isUserLoggedIn())
+    console.log('before ' + this.isUserLoggedIn());
     if (username === 'alex' && password === 'test') {
-      sessionStorage.setItem('authenticatedUser', username)
-      console.log('after ' + this.isUserLoggedIn())
-      return true
+      sessionStorage.setItem('authenticatedUser', username);
+      console.log('after ' + this.isUserLoggedIn());
+      return true;
     }
 
-    return false
+    return false;
   }
 
   isUserLoggedIn() {
-    let user = sessionStorage.getItem('authenticatedUser')
-    return !(user === null)
+    const user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
   }
 }
