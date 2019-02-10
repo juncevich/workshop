1606ddaa53daa6442599e7ed4baa6eed7b29730b
Alexandr Juncevich <a.juncevich@gmail.com>
2019-02-11 00:48:52 +0500

    Add DB

---
+++ java/spring/unofficial/udemy/Go Full Stack with Spring Boot and Angular 7/backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into todo(id, username, description, target_date, is_done)
+values (10001, 'Alex','Learn JPA', sysdate(), false);
+insert into todo(id, username, description, target_date, is_done)
+values (10002, 'Alex','Learn JPA 1', sysdate(), false);
+insert into todo(id, username, description, target_date, is_done)
+values (10003, 'Alex','Learn JPA 2', sysdate(), false);
