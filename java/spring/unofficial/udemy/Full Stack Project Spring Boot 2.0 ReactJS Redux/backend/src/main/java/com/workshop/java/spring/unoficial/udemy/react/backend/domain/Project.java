3f3a08d222c3b8d05c393fa3c1797801cf9f3d28
Aleksandr Juncevich <a.juncevich@gmail.com>
2019-06-02 15:09:46 +0300

    Start adding validation.

--- java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/backend/src/main/java/com/workshop/java/spring/unoficial/udemy/react/backend/domain/Project.java
+++ java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/backend/src/main/java/com/workshop/java/spring/unoficial/udemy/react/backend/domain/Project.java
@@ -1,11 +1,14 @@
 package com.workshop.java.spring.unoficial.udemy.react.backend.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -17,22 +20,31 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @NotBlank(message = "Project name is required")
     String projectName;
-    String projectNameIdentifier;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
+    String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     Date startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     Date endDate;
 
-    Date created_At;
-    Date updated_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    Date updatedAt;
 
     @PrePersist
     protected void onCreate() {
-        this.created_At = new Date();
+        this.createdAt = new Date();
     }
 
     @PreUpdate
     protected void onUpdate() {
-        this.updated_At = new Date();
+        this.updatedAt = new Date();
     }
 }
