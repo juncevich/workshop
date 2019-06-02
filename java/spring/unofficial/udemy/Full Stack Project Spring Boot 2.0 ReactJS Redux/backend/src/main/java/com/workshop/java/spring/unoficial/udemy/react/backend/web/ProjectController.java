415e16b305a9889ba9ccfe532b5309b09b721867
Aleksandr Juncevich <a.juncevich@gmail.com>
2019-06-02 16:05:42 +0300

    Add map validation service

--- java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/backend/src/main/java/com/workshop/java/spring/unoficial/udemy/react/backend/web/ProjectController.java
+++ java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/backend/src/main/java/com/workshop/java/spring/unoficial/udemy/react/backend/web/ProjectController.java
@@ -1,39 +1,36 @@
 package com.workshop.java.spring.unoficial.udemy.react.backend.web;
 
 import com.workshop.java.spring.unoficial.udemy.react.backend.domain.Project;
+import com.workshop.java.spring.unoficial.udemy.react.backend.services.MapValidationErrorService;
 import com.workshop.java.spring.unoficial.udemy.react.backend.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final MapValidationErrorService validationErrorService;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, MapValidationErrorService validationErrorService) {
         this.projectService = projectService;
+        this.validationErrorService = validationErrorService;
     }
 
     @PostMapping("")
     public ResponseEntity createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-            Map<String, String> errorsCollect = fieldErrors.stream()
-                    .filter(fieldError -> fieldError.getDefaultMessage() != null)
-                    .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
-            return new ResponseEntity<>(errorsCollect, HttpStatus.BAD_REQUEST);
+        Map<String, String> errorMap = validationErrorService.mapValidationService(bindingResult);
+        if (errorMap != null) {
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         Project savedProject = projectService.saveOrUpdateProject(project);
