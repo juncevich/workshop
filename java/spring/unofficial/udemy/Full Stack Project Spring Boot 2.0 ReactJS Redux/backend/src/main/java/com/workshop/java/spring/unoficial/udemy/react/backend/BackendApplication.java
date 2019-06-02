f4099bd233aa863a064e1168b977c0a62c3044ed
Aleksandr Juncevich <ayuncevich@unison.team>
2019-06-02 13:06:45 +0300

    Initial commit

---
+++ java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/backend/src/main/java/com/workshop/java/spring/unoficial/udemy/react/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.java.spring.unoficial.udemy.react.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
