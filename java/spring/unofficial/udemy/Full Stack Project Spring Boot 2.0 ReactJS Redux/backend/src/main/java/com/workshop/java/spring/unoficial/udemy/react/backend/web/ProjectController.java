6b8ef0893c8e19536d9e62ba3af60e21bff6a3ea
Aleksandr Juncevich <a.juncevich@gmail.com>
2019-06-02 15:25:04 +0300

    Add formatted error messages result.

--- java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/backend/src/main/java/com/workshop/java/spring/unoficial/udemy/react/backend/web/ProjectController.java
+++ java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/backend/src/main/java/com/workshop/java/spring/unoficial/udemy/react/backend/web/ProjectController.java
@@ -4,10 +4,13 @@ import com.workshop.java.spring.unoficial.udemy.react.backend.domain.Project;
 import com.workshop.java.spring.unoficial.udemy.react.backend.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,7 +23,11 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
+
         Project savedProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
     }
