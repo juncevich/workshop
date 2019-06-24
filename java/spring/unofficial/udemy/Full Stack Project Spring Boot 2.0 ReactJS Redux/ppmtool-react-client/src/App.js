ec70af8c821bc961000261c4fab4d452afbaa294
Alex Juncevich <a.juncevich@gmail.com>
2019-06-24 23:42:53 +0300

    Add update project form

--- java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/ppmtool-react-client/src/App.js
+++ java/spring/unofficial/udemy/Full Stack Project Spring Boot 2.0 ReactJS Redux/ppmtool-react-client/src/App.js
@@ -7,6 +7,7 @@ import AddProject from "./components/Project/AddProject";
 import Dashboard from "./components/Dashboard";
 import {Provider} from "react-redux"
 import store from "./store";
+import UpdateProject from "./components/Project/UpdateProject";
 
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
                         <Header/>
                         <Route exact path="/dashboard" component={Dashboard}/>
                         <Route exact path="/addProject" component={AddProject}/>
+                        <Route exact path="/updateProject/:id" component={UpdateProject}/>
                     </div>
                 </Router>
             </Provider>
