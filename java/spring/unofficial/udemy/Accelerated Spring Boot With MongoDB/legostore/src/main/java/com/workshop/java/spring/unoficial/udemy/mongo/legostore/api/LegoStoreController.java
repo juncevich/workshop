d6d51752c3e3c0e948d88cda632342fe2a1c29db
Alex Juncevich <a.juncevich@gmail.com>
2019-07-06 13:58:37 +0300

    Add delete

--- java/spring/unofficial/udemy/Accelerated Spring Boot With MongoDB/legostore/src/main/java/com/workshop/java/spring/unoficial/udemy/mongo/legostore/api/LegoStoreController.java
+++ java/spring/unofficial/udemy/Accelerated Spring Boot With MongoDB/legostore/src/main/java/com/workshop/java/spring/unoficial/udemy/mongo/legostore/api/LegoStoreController.java
@@ -2,6 +2,8 @@ package com.workshop.java.spring.unoficial.udemy.mongo.legostore.api;
 
 import com.workshop.java.spring.unoficial.udemy.mongo.legostore.model.LegoSet;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -18,6 +20,16 @@ public class LegoStoreController {
     @PostMapping
     public void insert(@RequestBody LegoSet legoSet) {
         this.mongoTemplate.insert(legoSet);
+    }
+
+    @PutMapping
+    public void update(@RequestBody LegoSet legoSet) {
+        this.mongoTemplate.save(legoSet);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        this.mongoTemplate.remove(new Query(Criteria.where("id").is(id)), LegoSet.class);
     }
 
     @GetMapping("/all")
