cf6252de8dee92e37355e66a0ad54d24832b1fdc
Alex Juncevich <a.juncevich@gmail.com>
2019-07-06 13:32:52 +0300

    Add insert

---
+++ java/spring/unofficial/udemy/Accelerated Spring Boot With MongoDB/legostore/src/main/java/com/workshop/java/spring/unoficial/udemy/mongo/legostore/api/LegoStoreController.java
@@ -0,0 +1,23 @@
+package com.workshop.java.spring.unoficial.udemy.mongo.legostore.api;
+
+import com.workshop.java.spring.unoficial.udemy.mongo.legostore.model.LegoSet;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("legostore/api")
+public class LegoStoreController {
+    private MongoTemplate mongoTemplate;
+
+    public LegoStoreController(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @PostMapping
+    public void insert(@RequestBody LegoSet legoSet) {
+        this.mongoTemplate.insert(legoSet);
+    }
+}
