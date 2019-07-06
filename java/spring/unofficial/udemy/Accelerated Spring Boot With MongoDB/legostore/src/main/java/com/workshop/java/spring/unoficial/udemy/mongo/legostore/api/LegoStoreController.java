67900c0c9425270b231adf39127bdf0162a76efe
Alex Juncevich <a.juncevich@gmail.com>
2019-07-06 13:38:30 +0300

    Add findAll + Postman request collection

--- java/spring/unofficial/udemy/Accelerated Spring Boot With MongoDB/legostore/src/main/java/com/workshop/java/spring/unoficial/udemy/mongo/legostore/api/LegoStoreController.java
+++ java/spring/unofficial/udemy/Accelerated Spring Boot With MongoDB/legostore/src/main/java/com/workshop/java/spring/unoficial/udemy/mongo/legostore/api/LegoStoreController.java
@@ -2,10 +2,9 @@ package com.workshop.java.spring.unoficial.udemy.mongo.legostore.api;
 
 import com.workshop.java.spring.unoficial.udemy.mongo.legostore.model.LegoSet;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("legostore/api")
@@ -20,4 +19,10 @@ public class LegoStoreController {
     public void insert(@RequestBody LegoSet legoSet) {
         this.mongoTemplate.insert(legoSet);
     }
+
+    @GetMapping("/all")
+    public Collection<LegoSet> all() {
+        return this.mongoTemplate.findAll(LegoSet.class);
+    }
+
 }
