598f5e254b6553eacc9f3bc6c8f6416fd7f46747
Alex Juncevich <a.juncevich@gmail.com>
2019-10-06 23:15:19 +0300

    Refactoring

--- react/monsters-rolodex/src/App.js
+++ react/monsters-rolodex/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
             monsters: [],
             searchField: ''
         }
+
     }
 
     componentDidMount() {
@@ -20,6 +21,10 @@ class App extends Component {
                 .then(users => this.setState({monsters: users}))
             )
     }
+
+    handleChange = (e) => {
+        this.setState({searchField: e.target.value});
+    };
 
     render() {
         const {monsters, searchField} = this.state;
@@ -30,7 +35,7 @@ class App extends Component {
             <div className="App">
                 <SearchBox
                     placeholder='search monsters'
-                    handleChange={e => this.setState({searchField: e.target.value})}
+                    handleChange={this.handleChange}
                 />
                 <CardList monsters={filteredMonsters}>
                 </CardList>
