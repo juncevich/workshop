93da0b64387926838b01207593243894871e93be
Alex Juncevich <a.juncevich@gmail.com>
2019-10-10 14:04:34 +0300

    Add custom button

--- react/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ react/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from "../form-input/form-input.component";
-
+import CustomButton from "../custom-button/custom-button.component";
 class SignIn extends React.Component {
 
     constructor(props) {
@@ -44,7 +44,7 @@ class SignIn extends React.Component {
                                label='password'
                                required/>
 
-                    <input type="submit" value='Submit Form'/>
+                    <CustomButton type="submit" value='Submit Form'>Sign in</CustomButton>
                 </form>
             </div>
         );
