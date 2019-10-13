5b943c69d983b435256814584fcba3b6b34102a5
Alex Juncevich <a.juncevich@gmail.com>
2019-10-13 17:24:11 +0300

    Improve Google auth

--- react/crwn-clothing/src/App.js
+++ react/crwn-clothing/src/App.js
@@ -9,7 +9,7 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-on-and-sign-up
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
@@ -18,20 +18,36 @@ class App extends React.Component {
     }
 
     unsubscribeFromAuth = null;
+
     componentDidMount() {
-        this.unsubscribeFromAuth =  auth.onAuthStateChanged(async user => {
-            createUserProfileDocument(user);
-        })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapShot => {
+                        this.setState({
+                            currentUser: {
+                                id: snapShot.id,
+                                ...snapShot.data()
+                            }
+                        });
+                    }
+                );
+
+            }
+
+            this.setState({currentUser: userAuth});
+        });
     }
 
     componentWillUnmount() {
         this.unsubscribeFromAuth();
     }
 
-    render(){
+    render() {
         return (
             <div>
-                <Header currentUser = {this.state.currentUser}/>
+                <Header currentUser={this.state.currentUser}/>
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route exact path='/shop' component={ShopPage}/>
