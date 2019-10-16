2ab1df9c74a5203472579a88d803fffcca328c59
Alex Juncevich <a.juncevich@gmail.com>
2019-10-16 21:27:01 +0300

    Add hidden to cart  component

--- react/crwn-clothing/src/components/header/header.component.jsx
+++ react/crwn-clothing/src/components/header/header.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './header.styles.scss';
-import {ReactComponent as Logo } from '../../assets/crown.svg';
+import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils'
 import {connect} from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -18,17 +18,21 @@ const Header = ({currentUser}) => (
 
             {
                 currentUser ?
-                    <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
                     :
                     <Link className='option' to='/signIn'>SIGN IN</Link>
             }
-            <CartIcon />
+            <CartIcon/>
         </div>
-        <CartDropdown/>
+
+        {
+            hidden ? null : <CartDropdown/>
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 export default connect(mapStateToProps)(Header);
