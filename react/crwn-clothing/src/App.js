4d8493a8bad3ef63c57ffde7495220a85e6637b9
Alexandr Juncevich <a.juncevich@gmail.com>
2019-10-26 00:21:20 +0300

    Add checkout page

--- react/crwn-clothing/src/App.js
+++ react/crwn-clothing/src/App.js
@@ -9,6 +9,7 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 class App extends React.Component {
 
@@ -46,6 +47,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route exact path='/shop' component={ShopPage}/>
+                    <Route exact path='/checkout' component={CheckoutPage}/>
                     <Route exact path='/signIn'
                            render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
                 </Switch>
--- react/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ react/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,23 +3,26 @@ import {connect} from 'react-redux';
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
-import { selectCartItems} from "../../redux/cart/cart.selector";
+import {selectCartItems} from "../../redux/cart/cart.selector";
 import './cart-dropdown.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const CartDropdown =({cartItems}) => (
-  <div className='cart-dropdown'>
-      <div className='cart-items'/>
-      {
-          cartItems.map(cartItem => (
-              <CartItem key={cartItem.id} item={cartItem}/>
-          ))
-      }
-      <CustomButton>GO TO CHECKOUT</CustomButton>
-  </div>
+const CartDropdown = ({cartItems, history}) => (
+    <div className='cart-dropdown'>
+        <div className='cart-items'/>
+        {
+            cartItems.length ?
+                cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem}/>
+                )) :
+                <span className='empty-message'>You cart is empty</span>
+        }
+        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    </div>
 );
 
 const mapStateToProps = state => ({
-   cartItems: selectCartItems(state)
+    cartItems: selectCartItems(state)
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
