15c861effc62da08142caa084263233a2da0de54
Alexandr Juncevich <a.juncevich@gmail.com>
2019-10-26 00:44:56 +0300

    Add dispatch hiding dropdown

--- react/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ react/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,8 +7,9 @@ import CartItem from "../cart-item/cart-item.component";
 import {selectCartItems} from "../../redux/cart/cart.selector";
 import './cart-dropdown.styles.scss';
 import {withRouter} from 'react-router-dom';
+import {toggleCartHidden} from "../../redux/cart/cart.actions";
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'/>
         {
@@ -18,7 +19,10 @@ const CartDropdown = ({cartItems, history}) => (
                 )) :
                 <span className='empty-message'>You cart is empty</span>
         }
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden())
+        }}>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
