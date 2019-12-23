b3a7cec0d57b49b0418b9da8ea3b7cc8e91cdb16
Alexandr Juncevich <a.juncevich@gmail.com>
2019-12-23 23:57:29 +0300

    Add directory state to Redux

--- react/crwn-clothing/src/redux/root-reducer.js
+++ react/crwn-clothing/src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import {combineReducers} from "redux";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import direcotryReducer from "./directory/directory.reducer";
 import {persistReducer} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -13,6 +14,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: direcotryReducer
 });
 export default persistReducer(persistConfig, rootReducer);
