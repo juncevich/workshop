1b33023d5fa2023123cf9c7b548969d88f98b9b6
Alexandr Juncevich <a.juncevich@gmail.com>
2019-12-26 23:58:55 +0300

    Add collection to selector

--- react/crwn-clothing/src/pages/collection/collection.component.jsx
+++ react/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({match}) => {
-    console.log(match.params.collectionId);
+import {connect} from 'react-redux';
+import {selectCollection} from "../../redux/shop/shop.selector";
+
+const CollectionPage = ({collection}) => {
+    console.log(collection);
     return (
         <div className='collection-page'>
             <h2>COLLECTION PAGE</h2>
@@ -11,4 +14,7 @@ const CollectionPage = ({match}) => {
     );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+export default connect(mapStateToProps)(CollectionPage);
