922908eeb7d8b2bb0f6a7309872eba478d14e415
Alexandr Juncevich <a.juncevich@gmail.com>
2019-12-26 23:19:07 +0300

    Add nested routes

--- react/crwn-clothing/src/pages/shop/shop.component.jsx
+++ react/crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CategoryPage from "../category/category.component";
 
-const ShopPage = ({collections}) => (
-    <div className='shop-page'>
-        <CollectionsOverview/>
-    </div>
-);
+const ShopPage = ({match}) => {
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+            <Route path={`${match.path}/:categoryId`} component={CollectionsOverview}/>
+        </div>
+    )
+};
 
 
 export default ShopPage;
