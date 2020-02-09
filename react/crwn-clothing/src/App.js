1ff28dd8c1958534311ea6001e5693bcf0245363
Alexandr Juncevich <a.juncevich@gmail.com>
2020-02-10 00:02:46 +0300

    Remove adding functional

--- react/crwn-clothing/src/App.js
+++ react/crwn-clothing/src/App.js
@@ -35,7 +35,6 @@ class App extends React.Component {
             }
 
             setCurrentUser(userAuth);
-            addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})));
         });
     }
 
