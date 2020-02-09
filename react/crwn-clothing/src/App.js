b785fcb9a6c7ee670579bb3e6b6d10d6ab83f864
Alexandr Juncevich <a.juncevich@gmail.com>
2020-02-09 23:56:18 +0300

    Remove adding functional

--- react/crwn-clothing/src/App.js
+++ react/crwn-clothing/src/App.js
@@ -6,20 +6,19 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {addCollectionAndDocuments, auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
 import {createStructuredSelector} from 'reselect';
 import {selectCurrentUser} from "./redux/user/user.selectors";
 import CheckoutPage from "./pages/checkout/checkout.component";
-import {selectCollectionsForPreview} from "./redux/shop/shop.selector";
 
 class App extends React.Component {
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        const {setCurrentUser, collectionsArray} = this.props;
+        const {setCurrentUser} = this.props;
 
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
@@ -64,7 +63,6 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser,
-    collectionsArray: selectCollectionsForPreview
 });
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
