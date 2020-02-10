8bf189e737df97b7f3a583736c877260a8da9373
Alexandr Juncevich <a.juncevich@gmail.com>
2020-02-10 23:41:47 +0300

    Fix problem with car icon

--- react/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ react/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
+import {createStructuredSelector} from 'reselect';
+
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
-import {selectCartItemsCount} from "../../redux/cart/cart.selector";
-import {createStructuredSelector} from "reselect";
-import {CartContainer, ItemCountContainer} from "./cart-icon.styles";
+import {selectCartItemsCount} from '../../redux/cart/cart.selector';
+
+import {CartContainer, ItemCountContainer, ShoppingIcon} from './cart-icon.styles';
 
 const CartIcon = ({toggleCartHidden, itemCount}) => (
     <CartContainer onClick={toggleCartHidden}>
