b6ba2ab4d898208a393f714b14f9c3d23e4b7f6d
Alexandr Juncevich <a.juncevich@gmail.com>
2020-02-24 23:49:17 +0300

    Add redux-saga

--- react/crwn-clothing/src/redux/shop/shop.sagas.js
+++ react/crwn-clothing/src/redux/shop/shop.sagas.js
@@ -1,8 +1,17 @@
-import {takeEvery} from "redux-saga/effects";
+import {call, put, takeEvery} from "redux-saga/effects";
 import ShopActionTypes from "./shop.types";
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
+import {fetchCollectionsSuccess} from "./shop.actions";
 
 export function* fetchCollections() {
-    yield console.log('I am fired');
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionMap));
+    } catch (error) {
+        yield put(error.message);
+    }
 }
 
 export function* fetchCollectionsStart() {
--- react/crwn-clothing/src/redux/store.js
+++ react/crwn-clothing/src/redux/store.js
@@ -4,9 +4,14 @@ import {persistStore} from "redux-persist";
 import rootReducer from './root-reducer';
 import createSagaMiddleware from 'redux-saga'
 import {fetchCollectionsStart} from "./shop/shop.sagas";
+import {logger} from "redux-logger/src";
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
