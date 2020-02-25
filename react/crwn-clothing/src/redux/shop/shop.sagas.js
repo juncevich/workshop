82d3b2402b1df1f4941d30c0f7d1d492e42742ea
Alexandr Juncevich <a.juncevich@gmail.com>
2020-02-25 23:51:14 +0300

    Add root saga

--- react/crwn-clothing/src/redux/shop/shop.sagas.js
+++ react/crwn-clothing/src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import {call, put, takeEvery} from "redux-saga/effects";
+import {call, put, takeLatest} from "redux-saga/effects";
 import ShopActionTypes from "./shop.types";
 import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
 import {fetchCollectionsSuccess} from "./shop.actions";
@@ -15,6 +15,6 @@ export function* fetchCollections() {
 }
 
 export function* fetchCollectionsStart() {
-    yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START,
+    yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START,
         fetchCollections);
 }
