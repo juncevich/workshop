18850394ee8d269b3ad71fc7c08c1aaa4d1c28b4
Alexandr Juncevich <a.juncevich@gmail.com>
2020-06-06 16:32:18 +0300

    Add repository and model

---
+++ java/spring/unofficial/minkowski/microservices/intro/src/main/kotlin/com/example/intro/domain/Person.kt
@@ -0,0 +1,8 @@
+package com.example.intro.domain
+
+data class Person(val id: Int,
+                  val firstName: String,
+                  val lastName: String,
+                  var age: Int) {
+
+}
