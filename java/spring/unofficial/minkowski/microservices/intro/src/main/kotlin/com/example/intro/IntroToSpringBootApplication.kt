d60f7dabc9d810e716098fde2c9e78cf1d897822
Alexandr Juncevich <a.juncevich@gmail.com>
2020-06-06 16:17:04 +0300

    initial commit Minkowski microservices intro

---
+++ java/spring/unofficial/minkowski/microservices/intro/src/main/kotlin/com/example/intro/IntroToSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.example.intro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntroToSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<IntroToSpringBootApplication>(*args)
+}
