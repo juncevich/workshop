c4f1e20659bee9d671090bcc012b4efe806afe44
Alexandr Juncevich <a.juncevich@gmail.com>
2020-06-08 22:53:56 +0300

    Drug & Drop initial commit

--- react/crwn-clothing/src/pages/collection/collection.component.jsx
+++ react/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import './collection.styles.scss';
 
 import {connect} from 'react-redux';
 import {selectCollection} from "../../redux/shop/shop.selector";
 import CollectionItem from "../../components/collection-item/collection-item.component";
+import {firestore} from "../../firebase/firebase.utils";
 
 const CollectionPage = ({collection}) => {
+    useEffect(() => {
+        const unsubscribeFromCollections = firestore.collection('collections').onSnapshot();
+            return () => {
+                unsubscribeFromCollections();
+            }
+        }
+    );
+
     const {title, items} = collection;
     return (
         <div className='collection-page'>
             <h2 className='title'>{title}</h2>
             <div className='items'>
-                {items.map( item => (
-                    <CollectionItem key={item.id} item={item}/>
+                {items.map(item => (
+                        <CollectionItem key={item.id} item={item}/>
                     )
                 )}
             </div>
