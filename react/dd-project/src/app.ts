394857a320ee09b0175ef20045f1b3b3ada77b43
Alexandr Juncevich <a.juncevich@gmail.com>
2020-06-10 22:56:51 +0300

    Add event listener

--- react/dd-project/src/app.ts
+++ react/dd-project/src/app.ts
@@ -3,6 +3,10 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,9 +14,24 @@ class ProjectInput {
 
         const importNode = document.importNode(this.templateElement.content, true);
         this.element = importNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input'
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
     }
 
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
+    }
 
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
