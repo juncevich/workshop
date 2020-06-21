0596c496861f602d9eab6a0c747dbe2a5bf8d754
Alexandr Juncevich <a.juncevich@gmail.com>
2020-06-21 19:29:04 +0300

    Add draggable functional

--- react/dd-project/src/app.ts
+++ react/dd-project/src/app.ts
@@ -1,3 +1,20 @@
+interface Draggable {
+
+    dragStartHandler(event: DragEvent): void;
+
+    dragEndHandler(event: DragEvent): void;
+}
+
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+
+    dropHandler(event: DragEvent): void;
+
+    dragLeaveHandler(event: DragEvent): void;
+}
+
+
 // Project Type
 enum ProjectStatus {
     Active,
@@ -157,7 +174,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
     private project: Project;
 
     get person() {
@@ -176,7 +193,19 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent();
     }
 
+    @autobind
+    dragStartHandler(event: DragEvent): void {
+        console.log(event);
+    }
+
+
+    dragEndHandler(_: DragEvent): void {
+        console.log('DrugEnd');
+    }
+
     configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler)
+        this.element.addEventListener('dragend', this.dragEndHandler)
     }
 
     renderContent() {
