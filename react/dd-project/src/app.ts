ce2484ccf49c81d043f80fc6e9202028cce5eeb5
Alexandr Juncevich <a.juncevich@gmail.com>
2020-06-21 18:52:06 +0300

    Add project item class

--- react/dd-project/src/app.ts
+++ react/dd-project/src/app.ts
@@ -11,7 +11,8 @@ class Project {
         public description: string,
         public people: number,
         public status: ProjectStatus
-    ) {}
+    ) {
+    }
 }
 
 // Project State Management
@@ -152,7 +153,30 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     }
 
     abstract configure(): void;
+
     abstract renderContent(): void;
+}
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {
+    }
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+
 }
 
 // ProjectList Class
@@ -193,9 +217,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         )! as HTMLUListElement;
         listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 }
@@ -224,7 +246,8 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
         this.element.addEventListener('submit', this.submitHandler);
     }
 
-    renderContent() {}
+    renderContent() {
+    }
 
     private gatherUserInput(): [string, string, number] | void {
         const enteredTitle = this.titleInputElement.value;
