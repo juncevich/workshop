0ee93510c5a3c577938730ebfb960d3454566542
Alexandr Juncevich <a.juncevich@gmail.com>
2020-06-21 20:07:33 +0300

    Finish drag and drop

--- react/dd-project/src/app.ts
+++ react/dd-project/src/app.ts
@@ -68,6 +68,18 @@ class ProjectState extends State<Project> {
             ProjectStatus.Active
         );
         this.projects.push(newProject);
+        this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        let project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
         }
@@ -239,8 +251,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     }
 
+    @autobind
     dropHandler(event: DragEvent): void {
-        console.log(event);
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
 
     @autobind
@@ -255,13 +269,12 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         this.element.addEventListener('drop', this.dropHandler);
 
         projectState.addListener((projects: Project[]) => {
-            const relevantProjects = projects.filter(prj => {
+            this.assignedProjects = projects.filter(prj => {
                 if (this.type === 'active') {
                     return prj.status === ProjectStatus.Active;
                 }
                 return prj.status === ProjectStatus.Finished;
             });
-            this.assignedProjects = relevantProjects;
             this.renderProjects();
         });
     }
