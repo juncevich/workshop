0e58c2ae278108982f2212817ecd6701a9b7b3e6
Alexandr Juncevich <a.juncevich@gmail.com>
2020-06-27 16:26:38 +0300

    Add context provider component

--- react/react-context-lesson/src/components/header/header.component.jsx
+++ react/react-context-lesson/src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
@@ -13,12 +13,11 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss';
 
 import CurrentUserContext from "../../contexts/current-user/current-user.context";
-import CartContext from "../../contexts/cart/cart.context";
+import {CartContext} from "../../providers/cart/cart.provider";
 
 const Header = () => {
     const currentUser = useContext(CurrentUserContext);
-    const [hidden, setHidden] = useState(true);
-    const toggleHidden = () => setHidden(!hidden);
+    const {hidden} = useContext(CartContext);
 
     return (
         <div className='header'>
@@ -41,12 +40,8 @@ const Header = () => {
                         SIGN IN
                     </Link>
                 )}
-                <CartContext.Provider value={{
-                    hidden,
-                    toggleHidden
-                }}>
+
                     <CartIcon/>
-                </CartContext.Provider>
             </div>
             {hidden ? null : <CartDropdown/>}
         </div>
