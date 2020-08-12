fc0e36b2d5e82567e3d80da964eb93738503e2fe
Alexandr Juncevich <a.juncevich@gmail.com>
2020-08-12 23:31:16 +0300

    Implement controller methods

---
+++ java/microservices/BreweryBeer/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,50 @@
+package guru.springframework.msscbeerservice.services;
+
+import guru.springframework.msscbeerservice.mappers.BeerMapper;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.web.controller.NotFoundException;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@Service
+@RequiredArgsConstructor
+public class BeerServiceImpl implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper     beerMapper;
+
+    @Override
+    public BeerDto getById(UUID beerId) {
+        return beerRepository.findById(beerId)
+                .map(beerMapper::entityToDto)
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return Stream.of(beerDto)
+                .map(beerMapper::dtoToEntity)
+                .map(beerRepository::save)
+                .map(beerMapper::entityToDto)
+                .findAny()
+                .orElseThrow();
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        final var beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+
+        beer.setBeerName(beerDto.getBeerName());
+        beer.setBeerStyle(beerDto.getBeerStyle().name());
+        beer.setPrice(beerDto.getPrice());
+        beer.setUpc(beerDto.getUpc());
+
+        final var savedBeer = beerRepository.save(beer);
+
+        return beerMapper.entityToDto(savedBeer);
+    }
+}
--- java/microservices/BreweryBeer/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ java/microservices/BreweryBeer/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,9 @@
 package guru.springframework.msscbeerservice.web.controller;
 
+import guru.springframework.msscbeerservice.services.BeerService;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -8,22 +11,28 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RequestMapping("api/v1/beer")
 @RestController
 public class BeerController {
 
+    private final BeerService beerService;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@Validated @PathVariable("beerId") UUID beerId) {
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        BeerDto beerDto = beerService.getById(beerId);
+        return new ResponseEntity<>(beerDto, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
-        return new ResponseEntity(HttpStatus.CREATED);
+    public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
+        BeerDto savedBeerDto = beerService.saveNewBeer(beerDto);
+        return new ResponseEntity<>(savedBeerDto, HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto) {
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto) {
+        BeerDto updatedBeerDto = beerService.updateBeer(beerId, beerDto);
+        return new ResponseEntity<>(updatedBeerDto, HttpStatus.NO_CONTENT);
     }
 }
