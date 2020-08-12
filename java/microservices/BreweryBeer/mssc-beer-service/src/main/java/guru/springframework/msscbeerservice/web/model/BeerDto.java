c819212e4fa2f92e0323b5a222ccc4b9d9212de1
Alexandr Juncevich <a.juncevich@gmail.com>
2020-08-12 22:29:57 +0300

    Add json format to BeerDto

--- java/microservices/BreweryBeer/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ java/microservices/BreweryBeer/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,8 +22,10 @@ public class BeerDto {
     @Null
     private Integer        version;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     @NotBlank
     @Size(min = 3, max = 100)
@@ -34,6 +37,7 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
