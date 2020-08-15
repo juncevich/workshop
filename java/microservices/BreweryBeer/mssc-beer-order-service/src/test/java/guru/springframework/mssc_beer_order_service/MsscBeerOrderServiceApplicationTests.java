ddf70aa955957d31108484b8c47e464a7d9693be
Alexandr Juncevich <a.juncevich@gmail.com>
2020-08-15 19:20:12 +0300

    Init beer order service

---
+++ java/microservices/BreweryBeer/mssc-beer-order-service/src/test/java/guru/springframework/mssc_beer_order_service/MsscBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc_beer_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
