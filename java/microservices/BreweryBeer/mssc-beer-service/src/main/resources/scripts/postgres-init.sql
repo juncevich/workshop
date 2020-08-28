3834ffd66e831dc23865b6eb1561ced8c8c9072f
Alexandr Juncevich <a.juncevich@gmail.com>
2020-08-28 20:36:29 +0300

    Remove script

--- java/microservices/BreweryBeer/mssc-beer-service/src/main/resources/scripts/postgres-init.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS beerservice;
-DROP USER IF EXISTS beer_service_user;
-CREATE USER beer PASSWORD 'beer_service_password';
-CREATE DATABASE beerservice;
-GRANT ALL PRIVILEGES ON DATABASE beerservice TO beer_service_user;
