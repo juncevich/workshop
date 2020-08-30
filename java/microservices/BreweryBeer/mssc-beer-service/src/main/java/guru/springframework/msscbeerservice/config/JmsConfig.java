8064b49341c70674f0a677549bee35d4ac669cbf
Alexandr Juncevich <a.juncevich@gmail.com>
2020-08-30 17:53:49 +0300

    Fix Jackson config to JMS

--- java/microservices/BreweryBeer/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ java/microservices/BreweryBeer/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -1,20 +1,25 @@
 package guru.springframework.msscbeerservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 public class JmsConfig {
     public static final String BREWING_REQUEST_QUEUE = "brewing-request";
     public static final String NEW_INVENTORY_QUEUE   = "brewing-request";
+
+    @Transactional
     @Bean
-    public MessageConverter jacksonJmsMessageConverter() {
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
         return converter;
     }
 }
