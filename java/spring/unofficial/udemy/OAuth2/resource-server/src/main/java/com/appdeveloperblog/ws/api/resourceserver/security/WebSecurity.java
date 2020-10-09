67409a84ea5b29ac48f403ee9e7927a4ef763ca6
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-09 19:59:11 +0300

    Add WebSecurity

---
+++ java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/security/WebSecurity.java
@@ -0,0 +1,8 @@
+package com.appdeveloperblog.ws.api.resourceserver.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+}
