cf33c2002acfcd8ea5d0f3489165532627936c5a
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-10 17:02:48 +0300

    Add ant machers

--- java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/security/WebSecurity.java
+++ java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package com.appdeveloperblog.ws.api.resourceserver.security;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/users").hasAnyAuthority("SCOPE_profile")
                 .anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer().jwt();
