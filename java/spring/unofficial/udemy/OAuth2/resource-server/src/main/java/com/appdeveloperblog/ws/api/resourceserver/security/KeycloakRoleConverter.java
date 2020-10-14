700f4f5caa5540653a6f7a88159afe2643ed126f
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-14 23:21:24 +0300

    Rename file

--- java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/security/KeycloakRoleConverter.java
+++ java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/security/KeycloakRoleConverter.java
@@ -6,7 +6,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.Collection;
 
-public class KeycloackRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
         return null;
