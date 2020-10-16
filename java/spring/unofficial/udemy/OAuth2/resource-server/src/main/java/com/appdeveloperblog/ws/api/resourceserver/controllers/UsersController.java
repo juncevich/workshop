2f3e626ac491a649d270b8836dfbe797c4207591
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-16 11:56:26 +0300

    Add postAuthorize

--- java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/controllers/UsersController.java
+++ java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.appdeveloperblog.ws.api.resourceserver.controllers;
 
 import com.appdeveloperblog.ws.api.resourceserver.response.UserRest;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -17,12 +18,12 @@ public class UsersController {
 
     //    @Secured("ROLE_developer")
     @PreAuthorize("hasAuthority('ROLE_developer') and #id==#jwt.subject")
-//    @PostAuthorize
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
     }
 
+    @PostAuthorize("returnObject.id==#jwt.subject")
     @GetMapping("/{id}")
     public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return UserRest.builder()
