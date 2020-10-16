e7aa544fe8fd7d2e8554ff2e6d9f5e5f27db084b
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-16 09:32:01 +0300

    Add enable global method security

--- java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/security/WebSecurity.java
+++ java/spring/unofficial/udemy/OAuth2/resource-server/src/main/java/com/appdeveloperblog/ws/api/resourceserver/security/WebSecurity.java
@@ -1,11 +1,13 @@
 package com.appdeveloperblog.ws.api.resourceserver.security;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
+@EnableGlobalMethodSecurity
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
