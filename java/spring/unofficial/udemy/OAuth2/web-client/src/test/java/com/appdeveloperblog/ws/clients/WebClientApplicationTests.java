39712c8a5a82543a7011ffe14b3ec1e7194e5ae7
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-20 17:39:19 +0300

    Add web client

---
+++ java/spring/unofficial/udemy/OAuth2/web-client/src/test/java/com/appdeveloperblog/ws/clients/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdeveloperblog.ws.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
