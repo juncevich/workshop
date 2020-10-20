8bd36a5aa6fba2c7b6ef38ec60830d8ffc9d1afd
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-21 01:49:36 +0300

    Add topbar

--- react/medium_clone/src/index.js
+++ react/medium_clone/src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from 'routes'
 import {BrowserRouter as Router} from 'react-router-dom'
-
+import TopBar from 'components/topBar'
 const App = () => {
     return (
         <div>
-            <h3>Welcome to hooks</h3>
             <Router>
+                <TopBar/>
                 <Routes/>
             </Router>
         </div>
     )
 }
-
 ReactDOM.render(
     <React.StrictMode>
         <App/>
