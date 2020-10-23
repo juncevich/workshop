96877fc6c790affc8eda2e35babf5def72c78e88
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-23 18:38:07 +0300

    Add WebClient bean to configuration

--- java/spring/unofficial/udemy/OAuth2/web-client/src/main/java/com/appdeveloperblog/ws/clients/WebClientApplication.java
+++ java/spring/unofficial/udemy/OAuth2/web-client/src/main/java/com/appdeveloperblog/ws/clients/WebClientApplication.java
@@ -2,19 +2,33 @@ package com.appdeveloperblog.ws.clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class WebClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient(ClientRegistrationRepository registrationRepository,
+                               OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(registrationRepository, oAuth2AuthorizedClientRepository);
+
+        oauth2.setDefaultOAuth2AuthorizedClient(true);
+        return WebClient.builder()
+                .apply(oauth2.oauth2Configuration())
+                .build();
+    }
 }
