d45d4d54032f8e91e1750777bd6bb24dd10f7901
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-23 17:44:20 +0300

    Add request to internal service

--- java/spring/unofficial/udemy/OAuth2/web-client/src/main/java/com/appdeveloperblog/ws/clients/controllers/AlbumsController.java
+++ java/spring/unofficial/udemy/OAuth2/web-client/src/main/java/com/appdeveloperblog/ws/clients/controllers/AlbumsController.java
@@ -1,7 +1,12 @@
 package com.appdeveloperblog.ws.clients.controllers;
 
 import com.appdeveloperblog.ws.clients.response.AlbumRest;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,16 +19,18 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 import static java.util.Optional.ofNullable;
 
 @Controller
+@RequiredArgsConstructor
 public class AlbumsController {
 
-    @Autowired
-    OAuth2AuthorizedClientService oAuth2ClientService;
+    final OAuth2AuthorizedClientService oAuth2ClientService;
+    final RestTemplate                  restTemplate;
 
     @GetMapping("/albums")
     public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
@@ -43,23 +50,28 @@ public class AlbumsController {
                 .map(AbstractOAuth2Token::getTokenValue)
                 .ifPresent(tokenValue -> System.out.println("idTokenValue = " + tokenValue));
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessTokenValue);
 
+        HttpEntity<List<AlbumRest>> httpEntity = new HttpEntity<>(headers);
+        String url = "http://localhost:8082/albums";
+        final ResponseEntity<List<AlbumRest>> responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<AlbumRest>>() {
+        });
+//        AlbumRest album1 = new AlbumRest();
+//        album1.setAlbumId("albumIdHere");
+//        album1.setUserId("1");
+//        album1.setAlbumTitle("Album 1 title");
+//        album1.setAlbumDescription("Album 1 description");
+//        album1.setAlbumUrl("Album 1 URL");
+//
+//        AlbumRest album2 = new AlbumRest();
+//        album2.setAlbumId("albumIdHere");
+//        album2.setUserId("2");
+//        album2.setAlbumTitle("Album 2 title");
+//        album2.setAlbumDescription("Album 2 description");
+//        album2.setAlbumUrl("Album 2 URL");
 
-        AlbumRest album1 = new AlbumRest();
-        album1.setAlbumId("albumIdHere");
-        album1.setUserId("1");
-        album1.setAlbumTitle("Album 1 title");
-        album1.setAlbumDescription("Album 1 description");
-        album1.setAlbumUrl("Album 1 URL");
-
-        AlbumRest album2 = new AlbumRest();
-        album2.setAlbumId("albumIdHere");
-        album2.setUserId("2");
-        album2.setAlbumTitle("Album 2 title");
-        album2.setAlbumDescription("Album 2 description");
-        album2.setAlbumUrl("Album 2 URL");
-
-        model.addAttribute("albums", List.of(album1, album2));
+        model.addAttribute("albums", responseEntity.getBody());
 
         return "albums";
     }
