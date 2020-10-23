55f0971d3021ad27e1d22d62b695833a453af1e4
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-23 16:01:44 +0300

    Add returning albums

--- java/spring/unofficial/udemy/OAuth2/web-client/src/main/java/com/appdeveloperblog/ws/clients/controllers/AlbumsController.java
+++ java/spring/unofficial/udemy/OAuth2/web-client/src/main/java/com/appdeveloperblog/ws/clients/controllers/AlbumsController.java
@@ -1,14 +1,33 @@
 package com.appdeveloperblog.ws.clients.controllers;
 
+import com.appdeveloperblog.ws.clients.response.AlbumRest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AlbumsController {
 
     @GetMapping("/albums")
     public String getAlbums(Model model) {
+        AlbumRest album1 = new AlbumRest();
+        album1.setAlbumId("albumIdHere");
+        album1.setUserId("1");
+        album1.setAlbumTitle("Album 1 title");
+        album1.setAlbumDescription("Album 1 description");
+        album1.setAlbumUrl("Album 1 URL");
+
+        AlbumRest album2 = new AlbumRest();
+        album2.setAlbumId("albumIdHere");
+        album2.setUserId("2");
+        album2.setAlbumTitle("Album 2 title");
+        album2.setAlbumDescription("Album 2 description");
+        album2.setAlbumUrl("Album 2 URL");
+
+        model.addAttribute("albums", List.of(album1, album2));
+
         return "albums";
     }
 }
---
+++ java/spring/unofficial/udemy/OAuth2/web-client/src/main/java/com/appdeveloperblog/ws/clients/response/AlbumRest.java
@@ -0,0 +1,12 @@
+package com.appdeveloperblog.ws.clients.response;
+
+import lombok.Data;
+
+@Data
+public class AlbumRest {
+    private String userId;
+    private String albumId;
+    private String albumTitle;
+    private String albumDescription;
+    private String albumUrl;
+}
