7c7baccd35ce7bbfc79e85cad75d359cf4ffc57e
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-27 21:09:51 +0300

    Add current user context

--- react/medium_clone/src/index.js
+++ react/medium_clone/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import Routes from 'routes'
 import {BrowserRouter as Router} from 'react-router-dom'
 import TopBar from 'components/topBar'
+import {CurrentUserProvider} from 'contexts/currentUser'
+
 const App = () => {
     return (
-        <div>
+        <CurrentUserProvider>
             <Router>
                 <TopBar/>
                 <Routes/>
             </Router>
-        </div>
+        </CurrentUserProvider>
     )
 }
 ReactDOM.render(
