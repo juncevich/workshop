a3012429a095e04d643c6919463b41fbe8d6a991
Alexandr Juncevich <a.juncevich@gmail.com>
2020-10-28 23:04:00 +0300

    Add get current user

--- react/medium_clone/src/index.js
+++ react/medium_clone/src/index.js
@@ -4,14 +4,17 @@ import Routes from 'routes'
 import {BrowserRouter as Router} from 'react-router-dom'
 import TopBar from 'components/topBar'
 import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
     return (
         <CurrentUserProvider>
-            <Router>
-                <TopBar/>
-                <Routes/>
-            </Router>
+            <CurrentUserChecker>
+                <Router>
+                    <TopBar/>
+                    <Routes/>
+                </Router>
+            </CurrentUserChecker>
         </CurrentUserProvider>
     )
 }
