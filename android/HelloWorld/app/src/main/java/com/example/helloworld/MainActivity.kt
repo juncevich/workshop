dc6183ce92c9bbb0bc6d480a751401ca622ee2fe
Alexandr Juncevich <a.juncevich@gmail.com>
2020-12-24 22:28:29 +0300

    Add people counter app

--- android/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ android/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.button)
+        val myTextView = findViewById<TextView>(R.id.textView)
+        var timeClicked =0
+        btnClickMe.setOnClickListener {
+            myTextView.text = timeClicked++.toString()
+            Toast.makeText(this@MainActivity, "you clicked me", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 }
