5d4aba9cef74b49c290cf103e4384598a41f2581
Alexandr Juncevich <a.juncevich@gmail.com>
2021-03-20 11:14:19 +0300

    Add docker compose

--- java/spring/unofficial/redis/backend/src/main/java/com/example/redis/backend/redis/model/User.java
+++ java/spring/unofficial/redis/backend/src/main/java/com/example/redis/backend/redis/model/User.java
@@ -1,12 +1,18 @@
 package com.example.redis.backend.redis.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @RedisHash("User")
 public class User implements Serializable {
 
