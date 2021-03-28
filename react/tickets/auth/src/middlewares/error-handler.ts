e15e3ae6f69c7bc18d93b165012cdf4f20f56e35
Alexandr Juncevich <a.juncevich@gmail.com>
2021-03-28 12:57:15 +0300

    Add determinining error type

--- react/tickets/auth/src/middlewares/error-handler.ts
+++ react/tickets/auth/src/middlewares/error-handler.ts
@@ -1,7 +1,17 @@
 import {NextFunction, Request, Response} from "express";
 
+import {RequestValidationError} from "../errors/request-validation-error";
+import {DatabaseConnectionError} from "../errors/database-connection-error";
+
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
+    if (err instanceof RequestValidationError){
+        console.log("Handling request as RequestValidationError")
+    }
+
+    if (err instanceof DatabaseConnectionError){
+        console.log("Handling request as DatabaseConnectionError")
+    }
     console.log('Something wrong', err);
 
     res.status(400).send({message: 'Something went wrong'});
