9a6a9e6872d31192073165f6e2db2f4bd68999bd
Alexandr Juncevich <a.juncevich@gmail.com>
2021-03-29 22:42:27 +0300

    Add connect to Mongo DB

--- react/tickets/auth/src/index.ts
+++ react/tickets/auth/src/index.ts
@@ -7,6 +7,7 @@ import {signOutRouter} from "./routes/signout";
 import {errorHandler} from "./middlewares/error-handler";
 import {NotFoundError} from "./errors/not-found-error";
 import 'express-async-errors';
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json())
@@ -20,7 +21,23 @@ app.all('*', async (req, res) => {
     throw new NotFoundError();
 })
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Connected to Mongo DB.')
+    } catch (err) {
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!');
+    })
+
+};
+
 app.use(errorHandler);
-app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-})
+
+start();
