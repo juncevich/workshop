d98869c385b214833dbef30514d369647a218e82
Alexandr Juncevich <a.juncevich@gmail.com>
2021-03-31 22:54:06 +0300

    Add user creation

--- react/tickets/auth/src/routes/signup.ts
+++ react/tickets/auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from "express-validator";
 import {RequestValidationError} from "../errors/request-validation-error";
-import {DatabaseConnectionError} from "../errors/database-connection-error";
+import {User} from "../models/user";
 
 const router = express.Router();
 
@@ -22,9 +22,17 @@ router.post('/api/users/signup', [
             throw new RequestValidationError(errors.array())
         }
         const {email, password} = req.body;
-        console.log('Creating user ...')
-        throw new DatabaseConnectionError();
-        res.send({});
+        const existingUser = await User.findOne({email});
+
+        if (existingUser) {
+            console.log('Email in use')
+            return res.send({});
+        }
+
+        const user = User.build({email, password});
+        await user.save();
+
+        res.status(201).send(user);
     });
 
 export {router as signUpRouter};
