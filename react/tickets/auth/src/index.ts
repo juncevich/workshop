ae0997e7c39e6ac879087aef02535b5a62931b4f
Alexandr Juncevich <a.juncevich@gmail.com>
2021-04-02 23:43:29 +0300

    Add cookie session

--- react/tickets/auth/src/index.ts
+++ react/tickets/auth/src/index.ts
@@ -8,9 +8,17 @@ import {errorHandler} from "./middlewares/error-handler";
 import {NotFoundError} from "./errors/not-found-error";
 import 'express-async-errors';
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
 
 app.use(currentUserRouter);
 app.use(signInRouter);
