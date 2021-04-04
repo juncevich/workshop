aa2208e7494a74f67fee68ed996ce82c5b58d77f
Alexandr Juncevich <a.juncevich@gmail.com>
2021-04-04 18:11:51 +0300

    Extract validation to middleware

--- react/tickets/auth/src/routes/signup.ts
+++ react/tickets/auth/src/routes/signup.ts
@@ -4,7 +4,7 @@ import {RequestValidationError} from "../errors/request-validation-error";
 import {User} from "../models/user";
 import {BadRequestError} from "../errors/bad-request-error";
 import jwt from 'jsonwebtoken';
-
+import {validateRequest} from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -17,13 +17,9 @@ router.post('/api/users/signup', [
             .isLength({min: 4, max: 20})
             .withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
 
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array())
-        }
         const {email, password} = req.body;
         const existingUser = await User.findOne({email});
 
