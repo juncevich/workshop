e89c38fa502424c24df7981f37685c57a06996dd
Alexandr Juncevich <a.juncevich@gmail.com>
2021-04-07 19:05:14 +0300

    App refactoring

--- react/tickets/auth/src/index.ts
+++ react/tickets/auth/src/index.ts
@@ -1,35 +1,6 @@
-import express from 'express';
-import {json} from 'body-parser';
-import {currentUserRouter} from "./routes/current-user";
-import {signInRouter} from "./routes/signin";
-import {signUpRouter} from "./routes/signup";
-import {signOutRouter} from "./routes/signout";
-import {errorHandler} from "./middlewares/error-handler";
-import {NotFoundError} from "./errors/not-found-error";
-import 'express-async-errors';
-import mongoose from "mongoose";
-import cookieSession from "cookie-session";
+import * as mongoose from "mongoose";
+import {app} from "./app";
 
-const app = express();
-app.set('trust proxy', true);
-app.use(json())
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-)
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signUpRouter);
-app.use(signOutRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-})
-
-app.use(errorHandler);
 
 const start = async () => {
     let jwtKey = process.env.JWT_KEY;
