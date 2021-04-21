a60b74ffdf696c61631d66141387e98a9b962a95
Alexandr Juncevich <a.juncevich@gmail.com>
2021-04-21 23:39:37 +0300

    Add error validation

--- react/tickets/client/pages/auth/signup.js
+++ react/tickets/client/pages/auth/signup.js
@@ -4,15 +4,20 @@ import axios from 'axios';
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        const response = await axios.post('/api/users/signup', {
-            email, password
-        });
+        try {
+            const response = await axios.post('/api/users/signup', {
+                email, password
+            });
 
-        console.log(response.data);
+            console.log(response.data);
+        } catch (err) {
+            setErrors(err.response.data.errors)
+        }
     }
     return (
         <form onSubmit={onSubmit}>
@@ -27,6 +32,13 @@ export default () => {
                 <input value={password} onChange={e => setPassword(e.target.value)} className="form-control"
                        type="password"/>
             </div>
+            {errors.length > 0 && (<div className="alert alert-danger">
+                <h4>Oooops......</h4>
+                <ul className="my-0">
+                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
+                </ul>
+
+            </div>)}
             <button className='btn btn-primary'>Signup</button>
         </form>
     );
