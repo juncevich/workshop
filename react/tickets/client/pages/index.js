38f357d674df72bc7037eaae017e8c808eaeab6a
Alexandr Juncevich <a.juncevich@gmail.com>
2021-05-01 22:27:40 +0300

    Add fetching data during ssr

--- react/tickets/client/pages/index.js
+++ react/tickets/client/pages/index.js
@@ -1,12 +1,15 @@
-const LandingPage =  ({color}) => {
+import acios from 'axios'
+
+const LandingPage =  ({currentUser}) => {
     console.log('I am in the component', color)
     return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps =() => {
-    console.log('I am in the server!');
+LandingPage.getInitialProps = async () => {
+    const response = await axios.get('/api/users/currentuser');
 
-    return {color: 'red'};
+
+    return response.data;
 }
 
 export default LandingPage;
