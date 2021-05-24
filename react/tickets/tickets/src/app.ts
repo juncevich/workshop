2cf86d7dcb3a1f81fc22cb00138fa80376400557
Alexandr Juncevich <a.juncevich@gmail.com>
2021-05-24 23:37:27 +0300

    Adding auth protection

--- react/tickets/tickets/src/app.ts
+++ react/tickets/tickets/src/app.ts
@@ -3,7 +3,8 @@ import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from 'cookie-session';
 import {createTicketRouter} from "./routes/new";
-import {NotFoundError} from "../../auth/src/errors/not-found-error";
+import {NotFoundError, } from "../../auth/src/errors/not-found-error";
+import {currentUser} from "../../auth/src/middlewares/current-user";
 import {errorHandler} from "../../auth/src/middlewares/error-handler";
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 );
+app.use(currentUser)
 app.use(createTicketRouter)
 
 app.all('*', async (req, res) => {
--- react/tickets/tickets/src/routes/__test__/new.test.ts
+++ react/tickets/tickets/src/routes/__test__/new.test.ts
@@ -10,7 +10,18 @@ it('has a route handler listening to api/tickets for post requests', async () =>
 })
 
 it('can be only accessed if the user is signed in ', async () => {
+    let response = await request(app)
+        .post('/api/tickets')
+        .send({})
+        .expect(401);
+})
 
+it('retuns a stats ither than 401 if the user is signed in', async () => {
+    let response = await request(app)
+        .post('/api/tickets')
+        .send({});
+
+    expect(response.status).not.toEqual(401)
 })
 
 it('returns an error if an invalid title is provided', async () => {
