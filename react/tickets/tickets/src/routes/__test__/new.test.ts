66137da1860de17392284515c42635a3565a5e73
Alexandr Juncevich <a.juncevich@gmail.com>
2021-05-31 23:23:39 +0300

    Add test

--- react/tickets/tickets/src/routes/__test__/new.test.ts
+++ react/tickets/tickets/src/routes/__test__/new.test.ts
@@ -1,37 +1,27 @@
-import request from "supertest";
-import {app} from "../../app";
+import request from 'supertest';
+import { app } from '../../app';
 
-it('has a route handler listening to api/tickets for post requests', async () => {
-    let response = await request(app)
+it('has a route handler listening to /api/tickets for post requests', async () => {
+    const response = await request(app).post('/api/tickets').send({});
+
+    expect(response.status).not.toEqual(404);
+});
+
+it('can only be accessed if the user is signed in', async () => {
+    await request(app).post('/api/tickets').send({}).expect(401);
+});
+
+it('returns a status other than 401 if the user is signed in', async () => {
+    const response = await request(app)
         .post('/api/tickets')
+        .set('Cookie', global.signin())
         .send({});
 
-    expect(response.status).not.toEqual(404)
-})
+    expect(response.status).not.toEqual(401);
+});
 
-it('can be only accessed if the user is signed in ', async () => {
-    let response = await request(app)
-        .post('/api/tickets')
-        .send({})
-        .expect(401);
-})
+it('returns an error if an invalid title is provided', async () => {});
 
-it('retuns a stats ither than 401 if the user is signed in', async () => {
-    let response = await request(app)
-        .post('/api/tickets')
-        .send({});
+it('returns an error if an invalid price is provided', async () => {});
 
-    expect(response.status).not.toEqual(401)
-})
-
-it('returns an error if an invalid title is provided', async () => {
-
-})
-
-it('returns an error if an invalid price is provided', async () => {
-
-})
-
-it('create a ticket with valid inputs', async () => {
-
-})
+it('creates a ticket with valid inputs', async () => {});
