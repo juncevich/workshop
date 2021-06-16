ed24b946ac59f7d60ba61f7cf4084029418ddfe3
Alexandr Juncevich <a.juncevich@gmail.com>
2021-06-16 13:42:05 +0300

    Add DB

--- go/udemy/go-auth/main.go
+++ go/udemy/go-auth/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"fmt"
+	"github.com/gofiber/fiber"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+
+	dsn := "host=localhost user=gorm_user password=gorm_password dbname=go_auth port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect with the database")
+	}
+
+	fmt.Print(db)
 	app := fiber.New()
 
 	app.Get("/", home)
