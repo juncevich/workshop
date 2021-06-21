ffcb43e730f6660b9e65c47b91be96f3750bccd8
Alexandr Juncevich <a.juncevich@gmail.com>
2021-06-21 23:48:17 +0300

    Add user struct

--- go/udemy/go-auth/controllers/authController.go
+++ go/udemy/go-auth/controllers/authController.go
@@ -1,7 +1,17 @@
 package controllers
 
-import "github.com/gofiber/fiber"
+import (
+	"../models"
+	"github.com/gofiber/fiber"
+)
 
-func Home(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+func Register(c *fiber.Ctx) error {
+
+	var user models.User
+
+	user.FirstName = "John"
+	user.LastName = "Doe"
+	user.Email = "john@doe.com"
+
+	return c.JSON(user)
 }
