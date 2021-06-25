1c6877bb3514bb6402e7ef14d3ff7b3c59c4a88c
Alexandr Juncevich <a.juncevich@gmail.com>
2021-06-25 17:41:45 +0300

    Add request data

--- go/udemy/go-auth/controllers/authController.go
+++ go/udemy/go-auth/controllers/authController.go
@@ -6,12 +6,25 @@ import (
 )
 
 func Register(c *fiber.Ctx) error {
+	var data map[string]string
 
-	var user models.User
+	err := c.BodyParser(&data)
+	if err != nil {
+		return err
+	}
 
-	user.FirstName = "John"
-	user.LastName = "Doe"
-	user.Email = "john@doe.com"
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Password do not match!",
+		})
+	}
+	user := models.User{
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+		Password:  data["password"],
+	}
 
 	return c.JSON(user)
 }
