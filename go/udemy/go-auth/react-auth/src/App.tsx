5ff075972793d8e977b71b09e08403f415b77506
Alexandr Juncevich <a.juncevich@gmail.com>
2021-07-12 23:52:22 +0300

    Add navigation

--- go/udemy/go-auth/react-auth/src/App.tsx
+++ go/udemy/go-auth/react-auth/src/App.tsx
@@ -4,11 +4,15 @@ import {BrowserRouter, Route} from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Nav from "./components/Nav";
 
 function App() {
     return (
         <div className="App">
+
             <BrowserRouter>
+                <Nav/>
+
                 <Route path="/" exact component={Home}/>
                 <Route path="/login" component={Login}/>
                 <Route path="/register" component={Register}/>
