17738cd671fdfe7a8d7889ead358351d7bf45e5d
Alexandr Juncevich <a.juncevich@gmail.com>
2021-07-13 23:47:33 +0300

    Add register

--- go/udemy/go-auth/react-auth/src/pages/Register.tsx
+++ go/udemy/go-auth/react-auth/src/pages/Register.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 
 const Login = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+
+
+    const submit = async () => {
+
+        const response = await axios.post('http://localhost:3000/api/register', {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            password: password,
+            password_confirm: passwordConfirm
+        })
+    }
     return (
-        <form className="form-signin">
+        <form className="form-signin" onSubmit={submit}>
             <h1 className="h3 mb-3 font-weight-normal">Please register</h1>
-            <input  className="form-control" placeholder="First Name" required/>
-            <input  className="form-control" placeholder="Last Name" required/>
-            <input type="email" className="form-control" placeholder="Email" required/>
-            <input type="password" className="form-control" placeholder="Password" required/>
-            <input type="password" className="form-control" placeholder="Password Confirm" required/>
+            <input className="form-control" placeholder="First Name" required
+                   onChange={e => setFirstName(e.target.value)}/>
+            <input className="form-control" placeholder="Last Name" required
+                   onChange={e => setLastName(e.target.value)}/>
+            <input type="email" className="form-control" placeholder="Email" required
+                   onChange={e => setEmail(e.target.value)}/>
+            <input type="password" className="form-control" placeholder="Password" required
+                   onChange={e => setPassword(e.target.value)}/>
+            <input type="password" className="form-control" placeholder="Password Confirm" required
+                   onChange={e => setPasswordConfirm(e.target.value)}/>
             <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button>
         </form>
     );
