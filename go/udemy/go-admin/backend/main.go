e80f81938520d3360ccdc4b86544f6d878aa9f4e
Alexandr Juncevich <a.juncevich@gmail.com>
2021-09-27 23:55:43 +0300

    Add fiber v2

--- go/udemy/go-admin/backend/main.go
+++ go/udemy/go-admin/backend/main.go
@@ -1,16 +1,13 @@
 package main
 
-import "github.com/gofiber/fiber"
+import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
 
-	app.Get(
-		"/",
-		func(c *fiber.Ctx) error {
-			return c.SendString("Hello, World 👋!")
-		},
-	)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 
-	app.Listen(":8000")
+	app.Listen(":3000")
 }
