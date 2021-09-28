acb3ddbbdde6df2eb03fa6341c3e4076aeadb7a0
Alexandr Juncevich <a.juncevich@gmail.com>
2021-09-29 00:11:00 +0300

    Add packages

---
+++ go/udemy/go-admin/backend/database/connect.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() {
+	dsn := "host=localhost user=gorm_user password=gorm_password dbname=gorm_db port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+	fmt.Println(db)
+
+}
--- go/udemy/go-admin/backend/main.go
+++ go/udemy/go-admin/backend/main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"backend/database"
+	"backend/routes"
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "host=localhost user=gorm_user password=gorm_password dbname=gorm_db port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	if err != nil {
-		panic("Could not connect to the database")
-	}
-	fmt.Println(db)
+	database.Connect()
 
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
-
+	routes.Setup(app)
 	app.Listen(":8000")
 }
