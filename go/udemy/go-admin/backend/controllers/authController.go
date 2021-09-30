6c535c6ab9441b59a59f0393e0d2e9403f8b897f
Alexandr Juncevich <a.juncevich@gmail.com>
2021-09-30 23:43:12 +0300

    Add user registration draft method logic

--- go/udemy/go-admin/backend/controllers/authController.go
+++ go/udemy/go-admin/backend/controllers/authController.go
@@ -6,9 +6,23 @@ import (
 )
 
 func Register(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "password do not match",
+		})
+	}
 	user := model.User{
-		FirstName: "John",
-		LastName:  "Doe",
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+		Password:  data["password"],
 	}
 	return c.JSON(user)
 }
