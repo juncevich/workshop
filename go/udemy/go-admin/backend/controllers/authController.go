f90a3fa6621a11fed3d5a438074a408573be34fc
Alexandr Juncevich <a.juncevich@gmail.com>
2021-11-05 01:12:09 +0300

    Add password methods to user

--- go/udemy/go-admin/backend/controllers/authController.go
+++ go/udemy/go-admin/backend/controllers/authController.go
@@ -5,7 +5,6 @@ import (
 	"backend/model"
 	"backend/util"
 	"github.com/gofiber/fiber/v2"
-	"golang.org/x/crypto/bcrypt"
 	"strconv"
 	"time"
 )
@@ -24,14 +23,13 @@ func Register(c *fiber.Ctx) error {
 		})
 	}
 
-	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 	user := model.User{
 		FirstName: data["first_name"],
 		LastName:  data["last_name"],
 		Email:     data["email"],
-		Password:  password,
 	}
 
+	user.SetPassword("password")
 	database.DB.Create(&user)
 	return c.JSON(user)
 }
@@ -54,7 +52,7 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+	if err := user.ComparePassword(data["password"]); err != nil {
 		c.Status(400)
 		return c.JSON(fiber.Map{
 			"message": "Incorrect password",
