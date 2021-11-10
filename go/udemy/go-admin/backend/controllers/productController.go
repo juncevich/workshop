8c8fbfa787147a7a8f2b4130dca197508d2f42cd
Alexandr Juncevich <a.juncevich@gmail.com>
2021-11-10 23:55:15 +0300

    Add products

---
+++ go/udemy/go-admin/backend/controllers/productController.go
@@ -0,0 +1,79 @@
+package controllers
+
+import (
+	"backend/database"
+	"backend/model"
+	"github.com/gofiber/fiber/v2"
+	"math"
+	"strconv"
+)
+
+func AllProducts(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	limit := 5
+	offset := (page - 1) * limit
+	var total int64
+
+	var products []model.Product
+
+	database.DB.Offset(offset).Limit(limit).Find(&products)
+	database.DB.Model(&model.Product{}).Count(&total)
+
+	return c.JSON(fiber.Map{
+		"data": products,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      page,
+			"last_page": math.Ceil(float64(int(total) / limit)),
+		},
+	})
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	var product model.Product
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	database.DB.Create(&product)
+
+	return c.JSON(product)
+}
+
+func GetProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := model.Product{
+		Id: uint(id),
+	}
+
+	database.DB.Find(&product)
+	return c.JSON(product)
+}
+
+func UpdateProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := model.Product{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	database.DB.Model(&product).Updates(product)
+	return c.JSON(product)
+}
+
+func DeleteProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := model.Product{
+		Id: uint(id),
+	}
+
+	database.DB.Delete(product)
+	return nil
+}
