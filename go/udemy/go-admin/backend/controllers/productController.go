9d6ea4384ae621d8b78619d4de080ea5ebf772b7
Alexandr Juncevich <a.juncevich@gmail.com>
2021-11-12 01:06:41 +0300

    Add interfaces

--- go/udemy/go-admin/backend/controllers/productController.go
+++ go/udemy/go-admin/backend/controllers/productController.go
@@ -4,29 +4,13 @@ import (
 	"backend/database"
 	"backend/model"
 	"github.com/gofiber/fiber/v2"
-	"math"
 	"strconv"
 )
 
 func AllProducts(c *fiber.Ctx) error {
 	page, _ := strconv.Atoi(c.Query("page", "1"))
-	limit := 5
-	offset := (page - 1) * limit
-	var total int64
 
-	var products []model.Product
-
-	database.DB.Offset(offset).Limit(limit).Find(&products)
-	database.DB.Model(&model.Product{}).Count(&total)
-
-	return c.JSON(fiber.Map{
-		"data": products,
-		"meta": fiber.Map{
-			"total":     total,
-			"page":      page,
-			"last_page": math.Ceil(float64(int(total) / limit)),
-		},
-	})
+	return c.JSON(model.Paginate(database.DB, &model.Product{}, page))
 }
 
 func CreateProduct(c *fiber.Ctx) error {
