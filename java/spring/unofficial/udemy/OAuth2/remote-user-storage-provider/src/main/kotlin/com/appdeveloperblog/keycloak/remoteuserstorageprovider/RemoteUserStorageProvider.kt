7f3b850ffa382fac96cff6600309b8514dd85e98
Alexandr Juncevich <a.juncevich@gmail.com>
2021-11-24 00:05:02 +0300

    Add RemoteUserStorageProvider class

---
+++ java/spring/unofficial/udemy/OAuth2/remote-user-storage-provider/src/main/kotlin/com/appdeveloperblog/keycloak/remoteuserstorageprovider/RemoteUserStorageProvider.kt
@@ -0,0 +1,38 @@
+package com.appdeveloperblog.keycloak.remoteuserstorageprovider
+
+import org.keycloak.credential.CredentialInput
+import org.keycloak.credential.CredentialInputValidator
+import org.keycloak.models.RealmModel
+import org.keycloak.models.UserModel
+import org.keycloak.storage.UserStorageProvider
+import org.keycloak.storage.user.UserLookupProvider
+
+class RemoteUserStorageProvider : UserStorageProvider, UserLookupProvider, CredentialInputValidator {
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserById(id: String?, realm: RealmModel?): UserModel {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserByUsername(username: String?, realm: RealmModel?): UserModel {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserByEmail(email: String?, realm: RealmModel?): UserModel {
+        TODO("Not yet implemented")
+    }
+
+    override fun supportsCredentialType(credentialType: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isConfiguredFor(realm: RealmModel?, user: UserModel?, credentialType: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isValid(realm: RealmModel?, user: UserModel?, credentialInput: CredentialInput?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
