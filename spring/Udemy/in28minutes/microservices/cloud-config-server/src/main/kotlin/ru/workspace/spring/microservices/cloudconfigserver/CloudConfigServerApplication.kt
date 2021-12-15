b0f1a13743cabfe20034227c8f6a76aabfd92631
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-15 22:35:38 +0300

    Add local config

--- spring/Udemy/in28minutes/microservices/cloud-config-server/src/main/kotlin/ru/workspace/spring/microservices/cloudconfigserver/CloudConfigServerApplication.kt
+++ spring/Udemy/in28minutes/microservices/cloud-config-server/src/main/kotlin/ru/workspace/spring/microservices/cloudconfigserver/CloudConfigServerApplication.kt
@@ -2,7 +2,9 @@ package ru.workspace.spring.microservices.cloudconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
 class CloudConfigServerApplication
 
