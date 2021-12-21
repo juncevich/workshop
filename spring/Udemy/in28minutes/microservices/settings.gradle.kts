9186675331a45fab53b38627c358b8e19a7f6500
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-21 14:10:41 +0300

    Add currency-conversion service

--- spring/Udemy/in28minutes/microservices/settings.gradle.kts
+++ spring/Udemy/in28minutes/microservices/settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "microservices_1"
 include(":limits-service")
 include(":cloud-config-server")
 include(":currency-exchange-service")
+include(":currency-conversion-service")
 
 pluginManagement {
     repositories {
