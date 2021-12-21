2f354167528cec28ddd1feb868abb6799fe0c457
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-22 01:38:50 +0300

    Add Open Feign client

--- spring/Udemy/in28minutes/microservices/currency-conversion-service/src/main/kotlin/ru/workspace/spring/microservices/currencyconversionservice/CurrencyConversionServiceApplication.kt
+++ spring/Udemy/in28minutes/microservices/currency-conversion-service/src/main/kotlin/ru/workspace/spring/microservices/currencyconversionservice/CurrencyConversionServiceApplication.kt
@@ -2,7 +2,9 @@ package ru.workspace.spring.microservices.currencyconversionservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 class CurrencyConversionServiceApplication
 
--- spring/Udemy/in28minutes/microservices/currency-conversion-service/src/main/kotlin/ru/workspace/spring/microservices/currencyconversionservice/rest/controllers/CurrencyConversionController.kt
+++ spring/Udemy/in28minutes/microservices/currency-conversion-service/src/main/kotlin/ru/workspace/spring/microservices/currencyconversionservice/rest/controllers/CurrencyConversionController.kt
@@ -3,13 +3,14 @@ package ru.workspace.spring.microservices.currencyconversionservice.rest.control
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.RestTemplate
+import ru.workspace.spring.microservices.currencyconversionservice.rest.clients.CurrencyExchangeServiceClient
 import ru.workspace.spring.microservices.currencyconversionservice.rest.dto.CurrencyConversionDto
-import ru.workspace.spring.microservices.currencyconversionservice.rest.dto.CurrencyExchangeDto
 import java.math.BigDecimal
 
 @RestController
-class CurrencyConversionController {
+class CurrencyConversionController(
+    val currencyExchangeServiceClient: CurrencyExchangeServiceClient
+) {
 
     @GetMapping("/currency-conversion/{from}/{to}/{quantity}")
     fun calculateCurrencyConversion(
@@ -18,17 +19,17 @@ class CurrencyConversionController {
         @PathVariable quantity: BigDecimal
     ): CurrencyConversionDto {
 
-        val uriVariables = mapOf(
-            "from" to from,
-            "to" to to
-        )
-        val currencyConversionResponseEntity = RestTemplate().getForEntity(
-            "http://localhost:8000/currency-exchange/{from}/{to}",
-            CurrencyExchangeDto::class.java,
-            uriVariables
-        )
-
-        val currencyConversionDto = currencyConversionResponseEntity.body ?: throw RuntimeException("Empty response")
+//        val uriVariables = mapOf(
+//            "from" to from,
+//            "to" to to
+//        )
+//        val currencyConversionResponseEntity = RestTemplate().getForEntity(
+//            "http://localhost:8000/currency-exchange/{from}/{to}",
+//            CurrencyExchangeDto::class.java,
+//            uriVariables
+//        )
+//        val currencyConversionDto = currencyConversionResponseEntity.body ?: throw RuntimeException("Empty response")
+        val currencyConversionDto = currencyExchangeServiceClient.exchangeCurrency(from, to)
         return CurrencyConversionDto(
             currencyConversionDto.id,
             from,
