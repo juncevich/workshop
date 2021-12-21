6dd6f178b99bcccb07be9eeea4ffaad30981568c
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-22 02:00:41 +0300

    Connect services to naming server

--- spring/Udemy/in28minutes/microservices/currency-conversion-service/src/main/kotlin/ru/workspace/spring/microservices/currencyconversionservice/rest/clients/CurrencyExchangeServiceClient.kt
+++ spring/Udemy/in28minutes/microservices/currency-conversion-service/src/main/kotlin/ru/workspace/spring/microservices/currencyconversionservice/rest/clients/CurrencyExchangeServiceClient.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import ru.workspace.spring.microservices.currencyconversionservice.rest.dto.CurrencyExchangeDto
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
 interface CurrencyExchangeServiceClient {
 
     @GetMapping("/currency-exchange/{from}/{to}")
