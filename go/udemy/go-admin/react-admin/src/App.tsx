4bdfd362043c012dbf55644bc314c84b49ed4a4b
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-26 22:49:10 +0300

    Add router

--- go/udemy/go-admin/react-admin/src/App.tsx
+++ go/udemy/go-admin/react-admin/src/App.tsx
@@ -2,43 +2,28 @@ import React from 'react';
 import './App.css';
 import Nav from "./components/Nav";
 import Menu from "./components/Menu";
+import Dashboard from "./pages/Dashboard";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Users from "./pages/Users";
 
 function App() {
     return (
         <div className="App">
-            <Nav/>
+            <BrowserRouter>
+                <Nav/>
+                <div className="container-fluid">
+                    <div className="row">
+                        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                            <Menu/>
+                            <Routes>
+                                <Route path="/"  element={<Dashboard/>}/>
+                                <Route path="/users" element={<Users/>}/>
+                            </Routes>
 
-            <div className="container-fluid">
-                <div className="row">
-                    <Menu/>
-
-                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-                        <div className="table-responsive">
-                            <table className="table table-striped table-sm">
-                                <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Header</th>
-                                    <th scope="col">Header</th>
-                                    <th scope="col">Header</th>
-                                    <th scope="col">Header</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>1,001</td>
-                                    <td>random</td>
-                                    <td>data</td>
-                                    <td>placeholder</td>
-                                    <td>text</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </main>
+                        </main>
+                    </div>
                 </div>
-            </div>
+            </BrowserRouter>
         </div>
     );
 }
--- go/udemy/go-admin/react-admin/src/components/Menu.tsx
+++ go/udemy/go-admin/react-admin/src/components/Menu.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 const Menu = () => {
     return (
         <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
             <div className="position-sticky pt-3">
-                <ul className="nav flex-column">
-                    <li className="nav-item">
-                        <a className="nav-link active">
+                <nav>
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link active">
+                                Dashboard
+                            </Link>
+                        </li>
 
-                            Dashboard
-                        </a>
-                    </li>
-                </ul>
+                        <li className="nav-item">
+                            <Link to="/users" className="nav-link active">
+                                Users
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </nav>
     );
