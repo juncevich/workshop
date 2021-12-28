a3623f60912da58d464b5c3759d4e5027ce651ae
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-29 00:23:45 +0300

    Add updating users

--- go/udemy/go-admin/react-admin/src/App.tsx
+++ go/udemy/go-admin/react-admin/src/App.tsx
@@ -6,6 +6,7 @@ import Users from "./pages/users/Users";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import UserCreate from "./pages/users/UserCreate";
+import UserEdit from "./pages/users/UserEdit";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/users" element={<Users/>}/>
                     <Route path="/users/create" element={<UserCreate/>}/>
+                    <Route path="/users/:id/edit" element={<UserEdit/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
