f25a738f0dad0a53e006966d10e1ebe8e836cc38
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-28 22:27:10 +0300

    Add user model

--- go/udemy/go-admin/react-admin/src/components/Nav.tsx
+++ go/udemy/go-admin/react-admin/src/components/Nav.tsx
@@ -1,17 +1,21 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
+import {User} from "../models/User";
 
 const Nav = () => {
-    const [user, setUser] = useState({
-        first_name: ''
-    })
+    const [user, setUser] = useState(new User())
 
     useEffect(() => {
         (
             async () => {
                 const {data} = await axios.get('user');
-                setUser(data)
+                setUser(new User(
+                    data.id,
+                    data.first_name,
+                    data.last_name,
+                    data.email,
+                ))
             }
         )()
     }, [])
@@ -26,7 +30,8 @@ const Nav = () => {
             <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
 
             <ul className="my-2 my-md-0 mr-md-3">
-                <Link to="/profile" className="p-2 text-white text-decoration-none">{user?.first_name}</Link>
+                <Link to="/profile"
+                      className="p-2 text-white text-decoration-none">{user.name}</Link>
                 <Link to="/login" className="p-2 text-white text-decoration-none" onClick={logout}>Sign
                     out</Link>
             </ul>
