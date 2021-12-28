620a33a135d2adc40c38bb1312b9a27e0117732e
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-28 22:30:28 +0300

    Add nav link

--- go/udemy/go-admin/react-admin/src/components/Menu.tsx
+++ go/udemy/go-admin/react-admin/src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 
 const Menu = () => {
@@ -9,15 +9,15 @@ const Menu = () => {
                 <nav>
                     <ul className="nav flex-column">
                         <li className="nav-item">
-                            <Link to="/" className="nav-link active">
+                            <NavLink to="/" className="nav-link">
                                 Dashboard
-                            </Link>
+                            </NavLink>
                         </li>
 
                         <li className="nav-item">
-                            <Link to="/users" className="nav-link active">
+                            <NavLink to="/users" className="nav-link">
                                 Users
-                            </Link>
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
