256e2d177746ab0d267cbdbd59b73714ee61d5e9
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-29 01:03:35 +0300

    Add role changing methods

--- go/udemy/go-admin/react-admin/src/App.tsx
+++ go/udemy/go-admin/react-admin/src/App.tsx
@@ -8,6 +8,8 @@ import Login from "./pages/Login";
 import UserCreate from "./pages/users/UserCreate";
 import UserEdit from "./pages/users/UserEdit";
 import Roles from "./pages/roles/Roles";
+import RoleCreate from "./pages/roles/RoleCreate";
+import RoleEdit from "./pages/roles/RoleEdit";
 
 function App() {
     return (
@@ -21,6 +23,8 @@ function App() {
                     <Route path="/users/create" element={<UserCreate/>}/>
                     <Route path="/users/:id/edit" element={<UserEdit/>}/>
                     <Route path="/roles" element={<Roles/>}/>
+                    <Route path="/roles/create" element={<RoleCreate/>}/>
+                    <Route path="/roles/:id/edit" element={<RoleEdit/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
