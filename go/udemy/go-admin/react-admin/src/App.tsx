0c3714261f8ee835a0ce0fcc7d0d8256e105395b
Alexandr Juncevich <a.juncevich@gmail.com>
2021-12-29 00:18:05 +0300

    Add creating users

--- go/udemy/go-admin/react-admin/src/App.tsx
+++ go/udemy/go-admin/react-admin/src/App.tsx
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Users from "./pages/users/Users";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import UserCreate from "./pages/users/UserCreate";
 
 function App() {
     return (
@@ -12,9 +13,10 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Dashboard/>}/>
-                    <Route path="/users" element={<Users/>}/>
                     <Route path="/register" element={<Register/>}/>
                     <Route path="/login" element={<Login/>}/>
+                    <Route path="/users" element={<Users/>}/>
+                    <Route path="/users/create" element={<UserCreate/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
