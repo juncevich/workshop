a353ac4a079911843649d9975632ba05e112556c
Alexandr Juncevich <a.juncevich@gmail.com>
2023-01-10 23:02:56 +0300

    Add app layout

--- go/udemy/go-movies/go-movies-frontend/src/App.tsx
+++ go/udemy/go-movies/go-movies-frontend/src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import Home from "./components/Home";
 
 function App() {
     return (
-        <div>Go watch a movie!</div>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1 className="mt-3">Go Watch a Movie!</h1>
+                </div>
+                <div className="col text-end">
+                    <a href="#!"><span className="badge bg-success">Login</span></a>
+                </div>
+                <hr className="mb-3"></hr>
+            </div>
+
+            <div className="row">
+                <div className="col-md-2">
+                    <nav>
+                        <div className="list-group">
+                            <a href="#!" className="list-group-item list-group-item-action">Home</a>
+                            <a href="#!" className="list-group-item list-group-item-action">Movies</a>
+                            <a href="#!" className="list-group-item list-group-item-action">Genres</a>
+                            <a href="#!" className="list-group-item list-group-item-action">Add Movie</a>
+                            <a href="#!" className="list-group-item list-group-item-action">Manage Catalogue</a>
+                            <a href="#!" className="list-group-item list-group-item-action">GraphQL</a>
+                        </div>
+                    </nav>
+                </div>
+                <div className="col-md-10">
+                    <Home/>
+                </div>
+            </div>
+        </div>
     );
 }
 
