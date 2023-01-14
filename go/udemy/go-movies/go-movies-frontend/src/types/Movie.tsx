6c250ba89735862a88177caf16159c46242118f5
Alexandr Juncevich <a.juncevich@gmail.com>
2023-01-15 01:26:37 +0300

    Create Movie component

---
+++ go/udemy/go-movies/go-movies-frontend/src/types/Movie.tsx
@@ -0,0 +1,10 @@
+type Movie = {
+    id: number;
+    title: string;
+    release_date: string;
+    runtime: number;
+    mpaa_rating: string;
+    description: string;
+};
+
+export default Movie;
