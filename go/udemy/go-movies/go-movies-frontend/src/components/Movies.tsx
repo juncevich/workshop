3b472b47e6ce2a31a62c0b17bed0e27cbda6fa73
Alexandr Juncevich <a.juncevich@gmail.com>
2023-01-15 19:40:41 +0300

    Connect frontend to backend

--- go/udemy/go-movies/go-movies-frontend/src/components/Movies.tsx
+++ go/udemy/go-movies/go-movies-frontend/src/components/Movies.tsx
@@ -8,26 +8,22 @@ const Movies = () => {
     const [movies, setMovies] = useState<Movie[]>([])
 
     useEffect(() => {
-            let movieList = [
-                {
-                    id: 1,
-                    title: "Highlander",
-                    release_date: "1986-03-07",
-                    runtime: 116,
-                    mpaa_rating: "R",
-                    description: "Super long description"
-                },
-                {
-                    id: 2,
-                    title: "Raiders of the Lost Ark",
-                    release_date: "1981-06-12",
-                    runtime: 115,
-                    mpaa_rating: "PG-13",
-                    description: "Some long description"
-                },
-            ];
+            const headers = new Headers()
+            headers.append("Content-Type", "application-json")
 
-            setMovies(movieList)
+            const requestOptions = {
+                method: "GET",
+                headers: headers,
+            }
+
+            fetch(`http://localhost:8080/movies`, requestOptions)
+                .then((response ) => response.json())
+                .then((data) => {
+                    setMovies(data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }, []
     );
 
