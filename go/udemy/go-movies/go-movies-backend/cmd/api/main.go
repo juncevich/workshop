e26ded3abffbce669605f419e58284247f7164ee
Alexandr Juncevich <a.juncevich@gmail.com>
2023-01-26 01:22:59 +0300

    Add defaulr JWT values

--- go/udemy/go-movies/go-movies-backend/cmd/api/main.go
+++ go/udemy/go-movies/go-movies-backend/cmd/api/main.go
@@ -6,14 +6,20 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = "8080"
 
 type application struct {
-	DSN    string
-	Domain string `json:"domain"`
-	DB     repository.DatabaseRepo
+	DSN          string
+	Domain       string `json:"domain"`
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 func main() {
@@ -21,6 +27,11 @@ func main() {
 	var app application
 	//read from commandline
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
 	flag.Parse()
 	// connect to database
 
@@ -31,7 +42,16 @@ func main() {
 	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
 	defer app.DB.Connection().Close()
 
-	app.Domain = "example.com"
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
 
 	log.Println("Starting application on port", port)
 
