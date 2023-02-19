136b360dba086dbffb7f8cc24e32137ecc38dd81
Alexandr Juncevich <a.juncevich@gmail.com>
2023-02-19 16:53:14 +0300

    Add text area component

---
+++ go/udemy/go-movies/go-movies-frontend/src/components/form/TextArea.tsx
@@ -0,0 +1,19 @@
+const TextArea = (props: any) => {
+    return (
+        <div className="mb-3">
+            <label className="form-label" htmlFor={props.name}>{props.title}</label>
+            <textarea
+                className="form-control"
+                id={props.name}
+                name={props.name}
+                value={props.value}
+                onChange={props.onChange}
+                rows={props.rows}
+            />
+
+            <div className={props.errorDiv}>{props.errorMsg}</div>
+        </div>
+    )
+}
+
+export default TextArea
