7ebc125b7c4fdff3204a00fe8153c27d8fedbd3c
Alexandr Juncevich <a.juncevich@gmail.com>
2023-02-21 23:22:27 +0300

    Add select component

---
+++ go/udemy/go-movies/go-movies-frontend/src/components/form/Select.tsx
@@ -0,0 +1,27 @@
+const Select = (props: any) => {
+    return (
+        <div className="mb-3">
+            <label className="form-label" htmlFor={props.name}>
+                {props.title}
+            </label>
+            <select
+                className="form-select"
+                name={props.name}
+                id={props.name}
+                value={props.value}
+                onChange={props.onChange}
+            >
+                <option value="">{props.placeholder}</option>
+                {props.options.map((option: any) => (
+                    <option
+                        key={option.id}
+                        value={option.id}
+                    >
+                        {option.value}
+                    </option>
+                ))}
+            </select>
+            <div className={props.errorDiv}>{props.errorMsg}</div>
+        </div>
+    )
+}
