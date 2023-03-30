3a9680d016478613186ac491c46ef094d7cec43a
Alexandr Juncevich <a.juncevich@gmail.com>
2023-03-30 23:18:14 +0300

    Creating a handler to list movies by Genre

--- go/udemy/go-movies/go-movies-backend/cmd/api/routes.go
+++ go/udemy/go-movies/go-movies-backend/cmd/api/routes.go
@@ -22,6 +22,7 @@ func (app *application) routes() http.Handler {
 	mux.Get("/movies/{id}", app.GetMovie)
 
 	mux.Get("/genres", app.AllGenres)
+	mux.Get("/movies/genres/{id}", app.AllMoviesByGenre)
 
 	mux.Route("/admin", func(r chi.Router) {
 		mux.Use(app.authRequired)
