254e8f00b62bc238baf09c7c8c74224c2a4c3511
Alexandr Juncevich <a.juncevich@gmail.com>
2024-01-08 02:14:30 +0300

    [Go Shive] - add auth middleware

---
+++ go/educative/shive/backend/middleware/authMiddleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	helper "backend/helpers"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthenticateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("name", claims.Name)
+		c.Set("username", claims.Username)
+		c.Set("uid", claims.Uid)
+		c.Set("user_type", claims.UserType)
+		c.Next()
+	}
+}
