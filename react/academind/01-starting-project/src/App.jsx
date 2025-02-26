c4ff9537a756e8701f2a8a43c7c832712b94fd88
Alexandr Juncevich <a.juncevich@gmail.com>
2025-02-26 23:23:25 +0300

    Add list elements outputting

--- react/academind/01-starting-project/src/App.jsx
+++ react/academind/01-starting-project/src/App.jsx
@@ -37,10 +37,7 @@ function App() {
             <main>
                 <section id="core-concepts">
                     <ul>
-                        <CoreConcept {...CORE_CONCEPTS[0]}/>
-                        <CoreConcept {...CORE_CONCEPTS[1]}/>
-                        <CoreConcept {...CORE_CONCEPTS[2]}/>
-                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                        {CORE_CONCEPTS.map(coreConcept => <CoreConcept key={coreConcept.title} {...coreConcept}/>)}
                     </ul>
                 </section>
                 <section id="examples">
