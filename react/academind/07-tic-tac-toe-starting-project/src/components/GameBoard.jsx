a2f12f3cc896775dcb8848520ad555eca5286d85
Alexandr Juncevich <a.juncevich@gmail.com>
2025-03-03 23:57:52 +0300

    Disable double clicking

--- react/academind/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ react/academind/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -20,7 +20,10 @@ export default function GameBoard({onSelectSquare, turns}) {
                             {row.map((playerSymbol, colIndex) => (
                                     <li key={colIndex}>
                                         {/*<button onClick={() => handleSelectSquare(rowIndex, colIndex)}>*/}
-                                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                                        <button
+                                            onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                            disabled={playerSymbol != null}
+                                        >
                                             {playerSymbol}
                                         </button>
                                     </li>
