2ba483c77a2db14c02e4da867dde1f1d39c190ae
Alexandr Juncevich <a.juncevich@gmail.com>
2025-03-06 00:15:20 +0300

    Add rematch

--- react/academind/07-tic-tac-toe-starting-project/src/App.jsx
+++ react/academind/07-tic-tac-toe-starting-project/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     const [gameTurns, setGameTurns] = useState([]);
     const activePlayer = deriveActivePlayer(gameTurns);
 
-    let gameBoard = initialGameBoard
+    let gameBoard = [...initialGameBoard.map(array => [...array])]
     for (const turn of gameTurns) {
         const {square, player} = turn;
         const {row, col} = square;
@@ -57,6 +57,10 @@ function App() {
         })
     }
 
+    function handleRematch() {
+        setGameTurns([]);
+    }
+
     return (
         <main>
             <div id="game-container">
@@ -64,7 +68,7 @@ function App() {
                     <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
                     <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
                 </ol>
-                {(winner || hasDraw) && <GameOver winner={winner}/>}
+                {(winner || hasDraw) && <GameOver winner={winner} onRestart={() => handleRematch()}/>}
                 <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
             </div>
             <Log turns={gameTurns}/>
